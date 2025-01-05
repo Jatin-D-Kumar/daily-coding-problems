2cf16c805202077c08cfaccc3894c1eab99bb65c
Jatin Kumar <jk8826984588@gmail.com>
2025-01-05 23:36:33 +0530

    Solved problem: Split the Array to Make Coprime Products - 2

--- problems/Split_the_Array_to_Make_Coprime_Products_2025-01-05.js
+++ problems/Split_the_Array_to_Make_Coprime_Products_2025-01-05.js
@@ -19,3 +19,13 @@ function splitArrayToMakeCoprimeProducts(arr) {
     for (let i = 1; i < (1 << n); i++) {
         const part1 = [];
         const part2 = [];
+
+        for (let j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                part1.push(arr[j]);
+            } else {
+                part2.push(arr[j]);
+            }
+        }
+
+        // C
