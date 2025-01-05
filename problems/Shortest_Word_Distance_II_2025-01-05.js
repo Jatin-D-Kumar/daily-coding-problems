c1e241c8948e5e76e348ce4e8d80e5bf4a6f3f07
Jatin Kumar <jk8826984588@gmail.com>
2025-01-05 23:49:28 +0530

    Solved problem: Shortest Word Distance II - 1

--- problems/Shortest_Word_Distance_II_2025-01-05.js
+++ problems/Shortest_Word_Distance_II_2025-01-05.js
@@ -8,4 +8,16 @@ class WordDistance {
             const word = wordsDict[i];
             if (!this.wordIndices.has(word)) {
                 this.wordIndices.set(word, []);
-            
+            }
+            this.wordIndices.get(word).push(i);
+        }
+    }
+
+    shortest(word1, word2) {
+        const indices1 = this.wordIndices.get(word1);
+        const indices2 = this.wordIndices.get(word2);
+        let minDistance = Infinity;
+        let i = 0, j = 0;
+
+        // Use two pointers to find the minimum distance
+        while (i < indices
