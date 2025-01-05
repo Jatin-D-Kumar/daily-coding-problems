4e2de341d22ee6b6c18c6cb05bd5fe9f262cc5d9
Jatin Kumar <jk8826984588@gmail.com>
2025-01-05 22:15:29 +0530

    Solved problem: Permutations II - 1

--- problems/Permutations_II_2025-01-05.js
+++ problems/Permutations_II_2025-01-05.js
@@ -8,4 +8,16 @@ function permuteUnique(nums) {
 
 function backtrack(nums, path, used, results) {
     if (path.length === nums.length) {
-        results.push([...path]); // Add a copy o
+        results.push([...path]); // Add a copy of the current path to results
+        return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        // Skip used numbers
+        if (used[i]) continue;
+
+        // Skip duplicates
+        if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue;
+
+        used[i] = true; // Mark the number as used
+        path.push(nums[i]); // Add the num
