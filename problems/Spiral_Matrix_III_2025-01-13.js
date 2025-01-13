03bc1e23069a76d600014b6ef3faef1aa213fae3
Jatin Kumar <jk8826984588@gmail.com>
2025-01-13 21:37:15 +0530

    Solved problem: Spiral Matrix III - 1

--- problems/Spiral_Matrix_III_2025-01-13.js
+++ problems/Spiral_Matrix_III_2025-01-13.js
@@ -5,4 +5,9 @@ function spiralMatrixIII(R, C, r0, c0) {
         [0, 1],   // right
         [1, 0],   // down
         [0, -1],  // left
- 
+        [-1, 0]   // up
+    ];
+    
+    let x = r0, y = c0;
+    let steps = 0; // Number of steps to take in the current direction
+    let directionIndex = 0; // Start wit
