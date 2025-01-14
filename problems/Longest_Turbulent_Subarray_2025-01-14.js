87c480923248bdf3577fff42a9c0a2c0ceed9873
Jatin Kumar <jk8826984588@gmail.com>
2025-01-14 22:07:45 +0530

    Solved problem: Longest Turbulent Subarray - 0

---
+++ problems/Longest_Turbulent_Subarray_2025-01-14.js
@@ -0,0 +1,9 @@
+
+function longestTurbulentSubarray(arr) {
+    if (arr.length === 0) return 0;
+
+    let maxLength = 1; // At least one element is a turbulent subarray
+    let currentLength = 1;
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > arr[i - 1] && (i === 1 || arr[i - 1] <= ar
