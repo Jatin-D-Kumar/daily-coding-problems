8d26b53b092a173fdb635faa81c2814c0ac9a7d9
Jatin Kumar <jk8826984588@gmail.com>
2025-01-15 21:19:43 +0530

    Solved problem: Daily Temperatures - 2

--- problems/Daily_Temperatures_2025-01-15.js
+++ problems/Daily_Temperatures_2025-01-15.js
@@ -4,4 +4,6 @@ function dailyTemperatures(temperatures) {
     const stack = []; // This will store the indices of the temperatures array
 
     for (let i = 0; i < temperatures.length; i++) {
-        // While there are indices in the stack and the current temperature
+        // While there are indices in the stack and the current temperature is greater
+        // than the temperature at the index stored at the top of the stack
+        while (stack.length > 0 && temperatures[i] > temperatures[s
