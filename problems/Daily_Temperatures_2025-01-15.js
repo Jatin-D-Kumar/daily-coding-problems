aa061535af2dc33d71474bf174b26b8a48c6e409
Jatin Kumar <jk8826984588@gmail.com>
2025-01-15 21:19:28 +0530

    Solved problem: Daily Temperatures - 1

--- problems/Daily_Temperatures_2025-01-15.js
+++ problems/Daily_Temperatures_2025-01-15.js
@@ -1,4 +1,7 @@
 
 function dailyTemperatures(temperatures) {
     const result = new Array(temperatures.length).fill(0);
-    const stack = []; // This will store the indices
+    const stack = []; // This will store the indices of the temperatures array
+
+    for (let i = 0; i < temperatures.length; i++) {
+        // While there are indices in the stack and the current temperature
