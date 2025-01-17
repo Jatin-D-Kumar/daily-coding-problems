084bd1e1c3b584708e56dcd0ef6760a98e29fd0c
Jatin Kumar <jk8826984588@gmail.com>
2025-01-17 22:14:31 +0530

    Solved problem: Can Convert String in K Moves - 1

--- problems/Can_Convert_String_in_K_Moves_2025-01-17.js
+++ problems/Can_Convert_String_in_K_Moves_2025-01-17.js
@@ -1,4 +1,10 @@
 
 function canConvertString(s1, s2, k) {
     // Check if the lengths of the strings are the same
- 
+    if (s1.length !== s2.length) {
+        return false;
+    }
+
+    let movesNeeded = 0;
+
+    // C
