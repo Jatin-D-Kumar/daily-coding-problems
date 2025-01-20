995009cda9101c9c4e7301393beb3874cbfe8962
Jatin Kumar <jk8826984588@gmail.com>
2025-01-20 21:00:43 +0530

    Solved problem: Best Time to Buy and Sell Stock II - 2

--- problems/Best_Time_to_Buy_and_Sell_Stock_II_2025-01-20.js
+++ problems/Best_Time_to_Buy_and_Sell_Stock_II_2025-01-20.js
@@ -3,4 +3,6 @@ function maxProfit(prices) {
     let totalProfit = 0;
 
     for (let i = 1; i < prices.length; i++) {
-        // If the current price is greater than the prev
+        // If the current price is greater than the previous price,
+        // we can make a profit by selling at the current price
+   
