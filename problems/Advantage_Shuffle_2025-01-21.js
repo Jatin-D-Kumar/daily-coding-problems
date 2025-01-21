c2417728c546db4c18464f1548c20a6bd709bf75
Jatin Kumar <jk8826984588@gmail.com>
2025-01-21 21:31:47 +0530

    Solved problem: Advantage Shuffle - 2

--- problems/Advantage_Shuffle_2025-01-21.js
+++ problems/Advantage_Shuffle_2025-01-21.js
@@ -10,4 +10,9 @@ function advantageShuffle(A, B) {
     
     let j = 0; // Pointer for B
     for (let i = 0; i < n; i++) {
-        // Find the first card in A that can be
+        // Find the first card in A that can beat the current card in B
+        while (j < n && A[i] <= B[j]) {
+            j++;
+        }
+        // If we found a card in A that can beat B[j], use it
+        if (j < 
