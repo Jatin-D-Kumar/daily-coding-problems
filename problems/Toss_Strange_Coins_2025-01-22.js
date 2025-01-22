30fe577d2c7175949d6208800d9601a1cec86c00
Jatin Kumar <jk8826984588@gmail.com>
2025-01-22 22:04:24 +0530

    Solved problem: Toss Strange Coins - 1

--- problems/Toss_Strange_Coins_2025-01-22.js
+++ problems/Toss_Strange_Coins_2025-01-22.js
@@ -3,4 +3,6 @@ function tossStrangeCoins(n, p) {
     const results = [];
     
     for (let i = 0; i < n; i++) {
-   
+        // Generate a random number between 0 and 1
+        const randomValue = Math.random();
+        
