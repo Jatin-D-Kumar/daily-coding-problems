969b3cc38b0543eb28e03036dcb28654a6063f08
Jatin Kumar <jk8826984588@gmail.com>
2025-01-23 22:12:38 +0530

    Solved problem: Max Consecutive Ones III - 2

--- problems/Max_Consecutive_Ones_III_2025-01-23.js
+++ problems/Max_Consecutive_Ones_III_2025-01-23.js
@@ -7,4 +7,8 @@ function longestOnes(nums, k) {
     for (let right = 0; right < nums.length; right++) {
         // If we encounter a 0, we increase the zero count
         if (nums[right] === 0) {
-          
+            zeroCount++;
+        }
+
+        // If the count of zeros exceeds k, we need to shrink the window
+        while (zeroCount > k)
