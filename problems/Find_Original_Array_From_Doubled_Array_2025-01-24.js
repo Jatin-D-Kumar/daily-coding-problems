ab62eb054d8fa2533f464f47dfe98ac18b299be7
Jatin Kumar <jk8826984588@gmail.com>
2025-01-25 16:58:17 +0530

    Solved problem: Find Original Array From Doubled Array - 1

--- problems/Find_Original_Array_From_Doubled_Array_2025-01-24.js
+++ problems/Find_Original_Array_From_Doubled_Array_2025-01-24.js
@@ -5,4 +5,10 @@ function findOriginalArray(changed) {
     const count = new Map();
     const original = [];
 
-    
+    // Count the occurrences of each number
+    for (const num of changed) {
+        count.set(num, (count.get(num) || 0) + 1);
+    }
+
+    // Sort the keys to process smaller numbers first
+   
