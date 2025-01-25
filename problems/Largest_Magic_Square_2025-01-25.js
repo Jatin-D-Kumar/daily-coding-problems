ab8c765671c960047df6b1fa7fc783957d9accc1
Jatin Kumar <jk8826984588@gmail.com>
2025-01-25 21:38:44 +0530

    Solved problem: Largest Magic Square - 1

--- problems/Largest_Magic_Square_2025-01-25.js
+++ problems/Largest_Magic_Square_2025-01-25.js
@@ -7,3 +7,14 @@ function isMagicSquare(square) {
     for (let i = 0; i < n; i++) {
         let rowSum = 0;
         let colSum = 0;
+        for (let j = 0; j < n; j++) {
+            rowSum += square[i][j];
+            colSum += square[j][i];
+        }
+        if (rowSum !== magicSum || colSum !== magicSum) {
+            return false;
+        }
+    }
+
+    // Check diagonals
+    let diag1Sum = 0;
