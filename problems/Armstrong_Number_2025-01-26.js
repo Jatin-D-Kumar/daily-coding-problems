4a3fa5151bc0af901139269b8ed4fc161165b926
Jatin Kumar <jk8826984588@gmail.com>
2025-01-26 21:10:27 +0530

    Solved problem: Armstrong Number - 1

--- problems/Armstrong_Number_2025-01-26.js
+++ problems/Armstrong_Number_2025-01-26.js
@@ -3,3 +3,8 @@ function isArmstrongNumber(num) {
     // Convert the number to a string to easily access each digit
     const strNum = num.toString();
     const numDigits = strNum.length;
+
+    // Calculate the sum of each digit raised to the power of the number of digits
+    let sum = 0;
+    for (let i = 0; i < numDigits; i++) {
+        const digit = parseInt
