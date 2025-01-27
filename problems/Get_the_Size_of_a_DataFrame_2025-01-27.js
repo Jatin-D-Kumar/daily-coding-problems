ec034b1aa2b383d6b70275a4c3cc9cf291e7959c
Jatin Kumar <jk8826984588@gmail.com>
2025-01-27 22:03:50 +0530

    Solved problem: Get the Size of a DataFrame - 1

--- problems/Get_the_Size_of_a_DataFrame_2025-01-27.js
+++ problems/Get_the_Size_of_a_DataFrame_2025-01-27.js
@@ -4,4 +4,9 @@ const danfo = require("danfojs-node");
 // Sample data
 const data = {
     column1: [1, 2, 3],
-    column2: ['A
+    column2: ['A', 'B', 'C'],
+    column3: [true, false, true]
+};
+
+// Create a DataFrame
+const df = new danfo.DataFrame(data);
