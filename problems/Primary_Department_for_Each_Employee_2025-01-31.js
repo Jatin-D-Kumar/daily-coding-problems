00a4f3dc21e21e0af5dc653bd8fe965bdf826550
Jatin Kumar <jk8826984588@gmail.com>
2025-01-31 10:17:33 +0530

    Solved problem: Primary Department for Each Employee - 1

--- problems/Primary_Department_for_Each_Employee_2025-01-31.js
+++ problems/Primary_Department_for_Each_Employee_2025-01-31.js
@@ -6,4 +6,12 @@ const employees = [
         name: "Alice",
         departments: [
             { name: "HR", priority: 2 },
-         
+            { name: "Finance", priority: 1 }
+        ]
+    },
+    {
+        id: 2,
+        name: "Bob",
+        departments: [
+            { name: "IT", priority: 3 },
+            { name
