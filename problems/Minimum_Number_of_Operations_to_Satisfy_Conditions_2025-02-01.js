dde7a9c83fbbe8e900bc9a2dea392d5a194adaf2
Jatin Kumar <jk8826984588@gmail.com>
2025-02-01 22:39:03 +0530

    Solved problem: Minimum Number of Operations to Satisfy Conditions - 2

--- problems/Minimum_Number_of_Operations_to_Satisfy_Conditions_2025-02-01.js
+++ problems/Minimum_Number_of_Operations_to_Satisfy_Conditions_2025-02-01.js
@@ -3,4 +3,6 @@ function minOperations(arr) {
     // Calculate the average of the array
     const average = Math.round(arr.reduce((a, b) => a + b, 0) / arr.length);
     
-    // Calculate the number of oper
+    // Calculate the number of operations needed to make all elements equal to the average
+    let operations = 0;
+    for (let nu
