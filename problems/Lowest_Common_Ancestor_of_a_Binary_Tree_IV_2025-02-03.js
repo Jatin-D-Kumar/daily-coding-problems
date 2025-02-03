34ddb63614ff2fd7b222ca00982da8f4b62c1afd
Jatin Kumar <jk8826984588@gmail.com>
2025-02-03 22:14:41 +0530

    Solved problem: Lowest Common Ancestor of a Binary Tree IV - 5

--- problems/Lowest_Common_Ancestor_of_a_Binary_Tree_IV_2025-02-03.js
+++ problems/Lowest_Common_Ancestor_of_a_Binary_Tree_IV_2025-02-03.js
@@ -46,4 +46,10 @@ function lowestCommonAncestor(root, nodes) {
 
 const root = new TreeNode(3);
 root.left = new TreeNode(5);
-root.right = new 
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(6);
+root.left.right = new TreeNode(2);
+root.right.left = new TreeNode(0);
+root.right.right = new TreeNode(8);
+root.left.right.left = new TreeNode(7);
+root.left.
