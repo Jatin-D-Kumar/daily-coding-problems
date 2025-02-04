c809c9c189c958320bb2d14ecffb89912a1d8bc1
Jatin Kumar <jk8826984588@gmail.com>
2025-02-04 21:36:08 +0530

    Solved problem: Longest Increasing Subsequence - 1

--- problems/Longest_Increasing_Subsequence_2025-02-04.js
+++ problems/Longest_Increasing_Subsequence_2025-02-04.js
@@ -3,4 +3,9 @@ function lengthOfLIS(nums) {
     if (nums.length === 0) return 0;
 
     // Create an array to store the length of the longest increasing subsequence
-    const dp
+    const dp = new Array(nums.length).fill(1);
+
+    // Fill the dp array
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nu
