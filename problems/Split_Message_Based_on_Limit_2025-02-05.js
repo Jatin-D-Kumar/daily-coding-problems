419cb22c38cd6e64b0defb8daf336b12ae86e660
Jatin Kumar <jk8826984588@gmail.com>
2025-02-05 21:04:13 +0530

    Solved problem: Split Message Based on Limit - 1

--- problems/Split_Message_Based_on_Limit_2025-02-05.js
+++ problems/Split_Message_Based_on_Limit_2025-02-05.js
@@ -5,4 +5,14 @@ function splitMessage(message, limit) {
     
     // Check if the message length is less than or equal to the limit
     if (message.length <= limit) {
-        me
+        messages.push(message);
+        return messages;
+    }
+
+    // Split the message into words
+    const words = message.split(' ');
+
+    // Initialize a temporary string to build the current message
+    let currentMessage = '';
+
+    // Iterate 
