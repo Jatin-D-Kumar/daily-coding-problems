100fcb95684aff1a36908acf3c0648e1ce9119d1
Jatin Kumar <jk8826984588@gmail.com>
2025-02-08 23:52:48 +0530

    Solved problem: Number of Different Subsequences GCDs - 1

--- problems/Number_of_Different_Subsequences_GCDs_2025-02-08.js
+++ problems/Number_of_Different_Subsequences_GCDs_2025-02-08.js
@@ -3,4 +3,6 @@ function countDifferentGCDs(nums) {
     const maxNum = Math.max(...nums);
     const gcdCount = new Array(maxNum + 1).fill(0);
     
-   
+    // Count occurrences of each number in the input array
+    const numCount = new Array(maxNum + 1).fill(0);
+    for (const num of nums)
