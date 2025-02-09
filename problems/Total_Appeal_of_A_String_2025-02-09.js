b131fce5f51d0e87d754667424c1f3dfc44760fc
Jatin Kumar <jk8826984588@gmail.com>
2025-02-09 21:01:10 +0530

    Solved problem: Total Appeal of A String - 3

--- problems/Total_Appeal_of_A_String_2025-02-09.js
+++ problems/Total_Appeal_of_A_String_2025-02-09.js
@@ -8,4 +8,12 @@ function totalAppeal(s) {
         // Calculate the contribution of s[i]
         // (i + 1) is the number of substrings that can start before or at i
         // (n - i) is the number of substrings that can end at or after i
-        total += (i + 1) * (n - i) * (s.charCodeAt(i) 
+        total += (i + 1) * (n - i) * (s.charCodeAt(i) - 'a'.charCodeAt(0) + 1);
+    }
+
+    return total;
+}
+
+// Example usage:
+const str = "abc";
+console.log(totalAppeal(str)); // Output: 14
