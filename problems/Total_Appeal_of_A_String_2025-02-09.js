c9dfa65d06664d3df6e22484b7c782fa8a5e8a23
Jatin Kumar <jk8826984588@gmail.com>
2025-02-09 21:00:55 +0530

    Solved problem: Total Appeal of A String - 2

--- problems/Total_Appeal_of_A_String_2025-02-09.js
+++ problems/Total_Appeal_of_A_String_2025-02-09.js
@@ -6,4 +6,6 @@ function totalAppeal(s) {
     // Iterate through each character in the string
     for (let i = 0; i < n; i++) {
         // Calculate the contribution of s[i]
-        // (i + 1) is the number of substrings that can start before 
+        // (i + 1) is the number of substrings that can start before or at i
+        // (n - i) is the number of substrings that can end at or after i
+        total += (i + 1) * (n - i) * (s.charCodeAt(i) 
