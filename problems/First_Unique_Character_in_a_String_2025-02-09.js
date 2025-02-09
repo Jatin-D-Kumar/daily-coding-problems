b6b21de2af96d4ab6e675ed2c0b30a94a5e85091
Jatin Kumar <jk8826984588@gmail.com>
2025-02-09 19:55:25 +0530

    Solved problem: First Unique Character in a String - 1

--- problems/First_Unique_Character_in_a_String_2025-02-09.js
+++ problems/First_Unique_Character_in_a_String_2025-02-09.js
@@ -2,4 +2,6 @@
 function firstUniqueCharacter(s) {
     const charCount = {};
 
-    // Count the occ
+    // Count the occurrences of each character
+    for (let char of s) {
+        charCount[char] = (cha
