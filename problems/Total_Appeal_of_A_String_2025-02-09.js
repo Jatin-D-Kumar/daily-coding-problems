a14dba88fa3f57181893741e94d54fe06e0b1472
Jatin Kumar <jk8826984588@gmail.com>
2025-02-09 21:00:39 +0530

    Solved problem: Total Appeal of A String - 1

--- problems/Total_Appeal_of_A_String_2025-02-09.js
+++ problems/Total_Appeal_of_A_String_2025-02-09.js
@@ -4,4 +4,6 @@ function totalAppeal(s) {
     const n = s.length;
 
     // Iterate through each character in the string
-    for (let 
+    for (let i = 0; i < n; i++) {
+        // Calculate the contribution of s[i]
+        // (i + 1) is the number of substrings that can start before 
