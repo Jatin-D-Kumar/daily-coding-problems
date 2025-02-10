f05d5b9e5188fe10c35557292b416bf516725c93
Jatin Kumar <jk8826984588@gmail.com>
2025-02-10 21:00:24 +0530

    Solved problem: Average of Levels in Binary Tree - 0

---
+++ problems/Average_of_Levels_in_Binary_Tree_2025-02-10.js
@@ -0,0 +1,14 @@
+
+class TreeNode {
+    constructor(val = 0, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+function averageOfLevels(root) {
+    if (!root) return [];
+
+    const result = [];
+    const queu
