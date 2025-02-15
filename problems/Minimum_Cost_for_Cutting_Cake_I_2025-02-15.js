707ad264d7528ed08dc1c59655dbd952ad8aebda
Jatin Kumar <jk8826984588@gmail.com>
2025-02-15 21:41:42 +0530

    Solved problem: Minimum Cost for Cutting Cake I - 1

--- problems/Minimum_Cost_for_Cutting_Cake_I_2025-02-15.js
+++ problems/Minimum_Cost_for_Cutting_Cake_I_2025-02-15.js
@@ -1,4 +1,8 @@
 
 function minCostToCutCake(costs) {
     const n = costs.length;
-    const dp = new Array(n + 1).fill
+    const dp = new Array(n + 1).fill(Infinity);
+    dp[0] = 0;
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = i - 1; j >= 0; j
