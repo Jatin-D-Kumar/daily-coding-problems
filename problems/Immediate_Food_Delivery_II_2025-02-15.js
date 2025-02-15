cc01648341c8b1c831f46f2df89fad31a3ec6a9f
Jatin Kumar <jk8826984588@gmail.com>
2025-02-15 21:44:16 +0530

    Solved problem: Immediate Food Delivery II - 1

--- problems/Immediate_Food_Delivery_II_2025-02-15.js
+++ problems/Immediate_Food_Delivery_II_2025-02-15.js
@@ -10,4 +10,20 @@ class Order {
 class FoodDelivery {
     constructor(timeLimit) {
         this.orders = [];
-        this.timeLimit = timeLimit; // maximum delivery tim
+        this.timeLimit = timeLimit; // maximum delivery time in minutes
+    }
+
+    addOrder(order) {
+        this.orders.push(order);
+    }
+
+    getImmediateDeliveries() {
+        return this.orders.filter(order => order.deliveryTime <= this.timeLimit);
+    }
+}
+
+// Example usage
+const deliveryService = new FoodDelivery(30); // 30 minutes time limit
+
+// Adding some orders
+deliveryService.add
