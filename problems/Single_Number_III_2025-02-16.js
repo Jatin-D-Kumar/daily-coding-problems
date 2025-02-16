39dfa97c3cb53a3a8902105d5bce0764a9bd66b0
Jatin Kumar <jk8826984588@gmail.com>
2025-02-16 20:53:23 +0530

    Solved problem: Single Number III - 2

--- problems/Single_Number_III_2025-02-16.js
+++ problems/Single_Number_III_2025-02-16.js
@@ -7,4 +7,6 @@ function singleNumber(nums) {
     }
 
     // Step 2: Find a bit that is set in the xor result
-  
+    let rightmostSetBit = xor & -xor;
+
+    // Step 3: Divide the numbers into two groups and find the un
