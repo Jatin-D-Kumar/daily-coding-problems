801a7e4f5967896c4c7d9c70f250df5fc6a176a7
Jatin Kumar <jk8826984588@gmail.com>
2025-02-19 21:44:15 +0530

    Solved problem: Climbing Stairs - 2

--- problems/Climbing_Stairs_2025-02-19.js
+++ problems/Climbing_Stairs_2025-02-19.js
@@ -6,4 +6,10 @@ function climbStairs(n) {
 
     let dp = new Array(n + 1);
     dp[0] = 1; // Base case: 1 way to stay at the ground (0 steps)
-    dp[1] = 1; // Base case: 1 way to climb 
+    dp[1] = 1; // Base case: 1 way to climb 1 step
+
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]; // The number of ways to reach step i
+    }
+
+   
