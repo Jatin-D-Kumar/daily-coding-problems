f327cfade6679965e573a78ad195d0a2c18a9a2a
Jatin Kumar <jk8826984588@gmail.com>
2025-02-19 21:44:00 +0530

    Solved problem: Climbing Stairs - 1

--- problems/Climbing_Stairs_2025-02-19.js
+++ problems/Climbing_Stairs_2025-02-19.js
@@ -4,4 +4,6 @@ function climbStairs(n) {
         return n; // If there are 1 or 2 steps, return n directly
     }
 
-    let dp =
+    let dp = new Array(n + 1);
+    dp[0] = 1; // Base case: 1 way to stay at the ground (0 steps)
+    dp[1] = 1; // Base case: 1 way to climb 
