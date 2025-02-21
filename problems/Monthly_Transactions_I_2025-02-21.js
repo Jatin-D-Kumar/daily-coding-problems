49aa7dc8e2a5308d25f3837fc1d3c424b794e3ae
Jatin Kumar <jk8826984588@gmail.com>
2025-02-21 21:13:33 +0530

    Solved problem: Monthly Transactions I - 1

--- problems/Monthly_Transactions_I_2025-02-21.js
+++ problems/Monthly_Transactions_I_2025-02-21.js
@@ -3,4 +3,9 @@ const transactions = [
     { date: '2023-01-15', amount: 100 },
     { date: '2023-01-20', amount: 200 },
     { date: '2023-02-10', amount: 150 },
-    { date:
+    { date: '2023-02-15', amount: 300 },
+    { date: '2023-03-05', amount: 250 },
+    { date: '2023-03-20', amount: 400 },
+];
+
+function getMonthlyTotals(transactions) {
