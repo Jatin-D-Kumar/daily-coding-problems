9393c1eaab5bc66f20f0d808a6185825fe744355
Jatin Kumar <jk8826984588@gmail.com>
2025-02-22 21:03:16 +0530

    Solved problem: Manager of the Largest Department - 1

--- problems/Manager_of_the_Largest_Department_2025-02-22.js
+++ problems/Manager_of_the_Largest_Department_2025-02-22.js
@@ -4,4 +4,10 @@ function managerOfLargestDepartment(employees) {
     const departmentManagers = {};
 
     // Loop through given employees to populate our objects
-    for (const employee of 
+    for (const employee of employees) {
+        const { manager, department } = employee;
+
+        // Count the size of the department
+        if (!departmentSizes[department]) {
+            departmentSizes[department] = 0;
+        
