a0d74894845671adc6b0ebcd71e4aaee3e1940e9
Jatin Kumar <jk8826984588@gmail.com>
2025-02-23 22:24:13 +0530

    Solved problem: Minimum Number of Keypresses - 1

--- problems/Minimum_Number_of_Keypresses_2025-02-23.js
+++ problems/Minimum_Number_of_Keypresses_2025-02-23.js
@@ -7,4 +7,10 @@ function minKeypresses(s) {
         charCount[char] = (charCount[char] || 0) + 1;
     }
 
-    // Create an array of frequ
+    // Create an array of frequencies and sort it in descending order
+    const frequencies = Object.values(charCount).sort((a, b) => b - a);
+
+    let keypresses = 0;
+    let pressesRequired = 1; // Number of presses required for the current key
+
+   
