5033e00529d0e5c7c71f3341020d45a3161d8611
Jatin Kumar <jk8826984588@gmail.com>
2025-02-26 22:07:13 +0530

    Solved problem: Count Number of Possible Root Nodes - 1

--- problems/Count_Number_of_Possible_Root_Nodes_2025-02-26.js
+++ problems/Count_Number_of_Possible_Root_Nodes_2025-02-26.js
@@ -3,4 +3,7 @@ function countPossibleRootNodes(edges) {
     // Create a set to keep track of all child nodes
     const childNodes = new Set();
 
-    /
+    // Iterate through the edges to populate the childNodes set
+    for (const [parent, child] of edges) {
+        childNodes.add(child);
+  
