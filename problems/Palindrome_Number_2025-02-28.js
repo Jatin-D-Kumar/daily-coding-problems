e3527d1f58241a24f8faf74ddc44d9dce0703f64
Jatin Kumar <jk8826984588@gmail.com>
2025-02-28 22:43:37 +0530

    Solved problem: Palindrome Number - 1

--- problems/Palindrome_Number_2025-02-28.js
+++ problems/Palindrome_Number_2025-02-28.js
@@ -3,4 +3,8 @@ function isPalindrome(num) {
     // Convert the number to a string
     const str = num.toString();
     
-    // 
+    // Initialize pointers for the start and end of the string
+    let left = 0;
+    let right = str.length - 1;
+    
+ 
