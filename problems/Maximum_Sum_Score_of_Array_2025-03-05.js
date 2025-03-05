82d0708b13bc09a9ea8e3fad7c296916bb6b9ffd
Jatin Kumar <jk8826984588@gmail.com>
2025-03-05 22:09:48 +0530

    Solved problem: Maximum Sum Score of Array - 1

--- problems/Maximum_Sum_Score_of_Array_2025-03-05.js
+++ problems/Maximum_Sum_Score_of_Array_2025-03-05.js
@@ -4,4 +4,5 @@ function maxSumScore(arr) {
         return 0; // No elements, no score
     }
     
-    let 
+    let maxSoFar = arr[0]; // This will hold the max sum found so far
+    let maxEndingHere = arr[0]; // This holds the max sum
