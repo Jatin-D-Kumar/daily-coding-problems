94e5fd948f05fae1f68211767520f7cb5d91a964
Jatin Kumar <jk8826984588@gmail.com>
2025-03-05 22:06:28 +0530

    Solved problem: Maximum Coin Collection  - 2

--- problems/Maximum_Coin_Collection__2025-03-05.js
+++ problems/Maximum_Coin_Collection__2025-03-05.js
@@ -10,3 +10,9 @@ function maxCoinCollection(grid) {
     dp[0][0] = grid[0][0];
 
     // Fill the first row (can only come from the left)
+    for (let j = 1; j < cols; j++) {
+        dp[0][j] = dp[0][j - 1] + grid[0][j];
+    }
+
+    // Fill the first column (can only come from above)
+    for (let i = 1; i < rows; 
