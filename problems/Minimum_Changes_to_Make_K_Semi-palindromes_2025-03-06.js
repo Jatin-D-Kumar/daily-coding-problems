0a2e794447c8983946e45e9845ebd516eb962b5f
Jatin Kumar <jk8826984588@gmail.com>
2025-03-06 21:47:40 +0530

    Solved problem: Minimum Changes to Make K Semi-palindromes - 1

--- problems/Minimum_Changes_to_Make_K_Semi-palindromes_2025-03-06.js
+++ problems/Minimum_Changes_to_Make_K_Semi-palindromes_2025-03-06.js
@@ -6,4 +6,15 @@ function minChangesToKSemiPalindromes(s, K) {
     function semiPalindromeChanges(start, end) {
         let changes = 0;
         while (start < end) {
-      
+            if (s[start] !== s[end]) {
+                changes++;
+            }
+            start++;
+            end--;
+        }
+        return changes;
+    }
+
+    // DP table
+    const dp = Array.from({ length: n + 1 }, () => Array(K + 1).fill(Infinity));
+    dp[0][0] = 0; // Bas
