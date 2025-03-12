d4c40fd0ba89e79adf401a9a5db1730db03435b4
Jatin Kumar <jk8826984588@gmail.com>
2025-03-12 21:39:01 +0530

    Solved problem: Maximum Fruits Harvested After at Most K Steps - 2

--- problems/Maximum_Fruits_Harvested_After_at_Most_K_Steps_2025-03-12.js
+++ problems/Maximum_Fruits_Harvested_After_at_Most_K_Steps_2025-03-12.js
@@ -8,4 +8,10 @@ function maxTotalFruits(fruits, startPos, k) {
         const minPos = fruits[left][0]; // leftmost position
         const maxPos = Math.min(fruits[left][0] + k, fruits[n-1][0]); // furthest right based on k steps
 
-        // Binary search to find the rightmost 
+        // Binary search to find the rightmost position we can collect fruits from
+        let right = left;
+        while (right < n - 1 && fruits[right + 1][0] <= maxPos) {
+            right++;
+        }
+
+        // Calculate the number of steps needed to
