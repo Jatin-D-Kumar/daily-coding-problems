4be917b0d03ae2f3f0274e6860554a2e35062b92
Jatin Kumar <jk8826984588@gmail.com>
2025-03-15 16:35:34 +0530

    Solved problem: Find Trending Hashtags - 1

--- problems/Find_Trending_Hashtags_2025-03-15.js
+++ problems/Find_Trending_Hashtags_2025-03-15.js
@@ -5,4 +5,12 @@ function findTrendingHashtags(hashtags, topN) {
 
     // Count occurrences of each hashtag
     for (const hashtag of hashtags) {
-     
+        if (hashtagCounts[hashtag]) {
+            hashtagCounts[hashtag]++;
+        } else {
+            hashtagCounts[hashtag] = 1;
+        }
+    }
+
+    // Convert the counts object into an array of entries [hashtag, count]
+   
