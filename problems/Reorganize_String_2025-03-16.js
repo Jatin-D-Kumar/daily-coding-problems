6ad1b719578aeb704a346b44b49d796719bcf62d
Jatin Kumar <jk8826984588@gmail.com>
2025-03-16 10:12:10 +0530

    Solved problem: Reorganize String - 1

--- problems/Reorganize_String_2025-03-16.js
+++ problems/Reorganize_String_2025-03-16.js
@@ -9,4 +9,14 @@ function reorganizeString(S) {
 
     // Create a max heap based on character frequency
     const maxHeap = [];
+    for (const char in charCount) {
+        maxHeap.push([-charCount[char], char]); // Push negative count for max heap
+    }
+    maxHeap.sort(); // Sort to create a max heap
+
+    let result = '';
+    let prevChar = null;
+    let prevCount = 0;
+
+    while (maxHeap.length > 0) {
     
