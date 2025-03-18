69a5aae92b7a16c181c05395543e9f93f45f71a3
Jatin Kumar <jk8826984588@gmail.com>
2025-03-18 22:25:04 +0530

    Solved problem: Minimum Insertion Steps to Make a String Palindrome - 2

--- problems/Minimum_Insertion_Steps_to_Make_a_String_Palindrome_2025-03-18.js
+++ problems/Minimum_Insertion_Steps_to_Make_a_String_Palindrome_2025-03-18.js
@@ -10,4 +10,14 @@ function minInsertions(s) {
             if (s[i] === s[j]) {
                 dp[i][j] = dp[i + 1][j - 1];
             } else {
-             
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+            }
+        }
+    }
+
+    return dp[0][n - 1];
+}
+
+// Example usage:
+const str = "abc";
+console.log(minInsertions(str)); // Output: 2
