0bd99f59b317b8e7907fadfcb7ef6ffab1ea6119
Jatin Kumar <jk8826984588@gmail.com>
2025-03-19 21:46:19 +0530

    Solved problem: Search a 2D Matrix - 3

--- problems/Search_a_2D_Matrix_2025-03-19.js
+++ problems/Search_a_2D_Matrix_2025-03-19.js
@@ -22,4 +22,15 @@ function searchMatrix(matrix, target) {
         }
     }
 
-    re
+    return false;
+}
+
+// Example usage:
+const matrix = [
+    [1, 3, 5, 7],
+    [10, 11, 16, 20],
+    [23, 30, 34, 60]
+];
+const target = 3;
+
+console.log(searchMatrix(matrix, target)); // Output: true
