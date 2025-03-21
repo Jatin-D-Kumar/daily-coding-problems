9f32ac85c3bb995e973ba6acf8725a8964adbbda
Jatin Kumar <jk8826984588@gmail.com>
2025-03-21 22:51:57 +0530

    Solved problem: Rising Temperature - 1

--- problems/Rising_Temperature_2025-03-21.js
+++ problems/Rising_Temperature_2025-03-21.js
@@ -4,4 +4,8 @@ function dailyTemperatures(temperatures) {
     const stack = []; // This will store the indexes of the temperatures
 
     for (let i = 0; i < temperatures.length; i++) {
-        // While there is a stack and the current temperature is greater than the tem
+        // While there is a stack and the current temperature is greater than the temperature
+        // at the index stored at the top of the stack
+        while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            const idx = stack.pop(); // Get index of the last stored temperature
+            result[idx] = i - idx; // Calculate the number of days until a 
