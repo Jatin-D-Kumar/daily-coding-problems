57f2da571f4f94b7dbd4c3cbab417ffc1ee0c5e4
Jatin Kumar <jk8826984588@gmail.com>
2025-03-22 22:26:31 +0530

    Solved problem: Permutations IV - 0

---
+++ problems/Permutations_IV_2025-03-22.js
@@ -0,0 +1,9 @@
+
+function permuteUnique(nums) {
+    const results = [];
+    nums.sort((a, b) => a - b); // Sort the array to handle duplicates
+    backtrack(nums, [], Array(nums.length).fill(false), results);
+    return results;
+}
+
+function backtrack(nums, path, u
