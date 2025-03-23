7e91e18d3bc623fe3db80b65280fa5ac4e241c14
Jatin Kumar <jk8826984588@gmail.com>
2025-03-23 21:00:40 +0530

    Solved problem: Right Triangles - 2

--- problems/Right_Triangles_2025-03-23.js
+++ problems/Right_Triangles_2025-03-23.js
@@ -4,4 +4,7 @@ function isRightTriangle(a, b, c) {
     const sides = [a, b, c].sort((x, y) => x - y);
     
     // Assign the sides to variables
-    const [side1, side2, hypotenu
+    const [side1, side2, hypotenuse] = sides;
+
+    // Check the Pythagorean theorem
+    return (side1 * side1 + side2 * side2) === (hypotenuse * hy
