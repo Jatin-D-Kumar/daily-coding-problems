92c2151e4c0044ce416b7f1a69f7861d56874d19
Jatin Kumar <jk8826984588@gmail.com>
2025-03-30 21:36:06 +0530

    Solved problem: RLE Iterator - 1

--- problems/RLE_Iterator_2025-03-30.js
+++ problems/RLE_Iterator_2025-03-30.js
@@ -8,4 +8,12 @@ class RLEIterator {
 
     next(n) {
         while (this.index < this.A.length) {
-            // If we have no remaini
+            // If we have no remaining count, move to the next value
+            if (this.count === 0) {
+                this.count = this.A[this.index]; // Get the count of the current value
+                this.index += 1; // Move to the next value
+            }
+
+            // If n is less than or equal to the remaining count
+            if (n <= this.count) {
+ 
