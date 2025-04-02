2fcf17c8fa006eb8ae70040ba23934f6c11a75c3
Jatin Kumar <jk8826984588@gmail.com>
2025-04-02 21:19:11 +0530

    Solved problem: Maximum Points After Enemy Battles - 1

--- problems/Maximum_Points_After_Enemy_Battles_2025-04-02.js
+++ problems/Maximum_Points_After_Enemy_Battles_2025-04-02.js
@@ -2,4 +2,7 @@
 function maxPointsAfterBattles(battles) {
     let maxPoints = 0;
 
-    for (let i = 0; i < battles.length; i++
+    for (let i = 0; i < battles.length; i++) {
+        if (battles[i] > 0) {
+            maxPoints += battles[i]; // Add positive points
+        } else i
