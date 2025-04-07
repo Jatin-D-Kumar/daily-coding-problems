c072db10cca305cc06c66a68f86de1487ffa12ed
Jatin Kumar <jk8826984588@gmail.com>
2025-04-07 21:41:30 +0530

    Solved problem: Simplified Fractions - 1

--- problems/Simplified_Fractions_2025-04-07.js
+++ problems/Simplified_Fractions_2025-04-07.js
@@ -11,4 +11,7 @@ function gcd(a, b) {
 function simplifiedFractions(n) {
     const result = [];
     
-    for (let denominat
+    for (let denominator = 2; denominator <= n; denominator++) {
+        for (let numerator = 1; numerator < denominator; numerator++) {
+            if (gcd(numerator, denominator) === 1) {
+                result.push(`${numerator}/
