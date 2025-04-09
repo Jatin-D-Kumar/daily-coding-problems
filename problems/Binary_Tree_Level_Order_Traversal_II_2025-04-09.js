0129fc566d127533d2ff088ee573b1bfd76f059d
Jatin Kumar <jk8826984588@gmail.com>
2025-04-09 21:42:23 +0530

    Solved problem: Binary Tree Level Order Traversal II - 3

--- problems/Binary_Tree_Level_Order_Traversal_II_2025-04-09.js
+++ problems/Binary_Tree_Level_Order_Traversal_II_2025-04-09.js
@@ -12,3 +12,7 @@ function levelOrderBottom(root) {
         for (let i = 0; i < levelSize; i++) {
             const node = queue.shift();
             currentLevel.push(node.val);
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
