fbd43f4f7650e2ec352ddd8f2e92810aa507e330
Jatin Kumar <jk8826984588@gmail.com>
2025-04-13 21:00:33 +0530

    Solved problem: Maximum Prime Difference - 2

--- problems/Maximum_Prime_Difference_2025-04-13.js
+++ problems/Maximum_Prime_Difference_2025-04-13.js
@@ -5,4 +5,10 @@ function isPrime(num) {
     if (num % 2 === 0 || num % 3 === 0) return false;
     
     for (let i = 5; i * i <= num; i += 6) {
-        if (num % i =
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+}
+
+function maxPrimeDifference(arr) {
+  
