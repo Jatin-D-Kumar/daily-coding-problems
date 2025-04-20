ef09e6cfa215e4f66f42db6b9e56e9a2073d717c
Jatin Kumar <jk8826984588@gmail.com>
2025-04-20 21:18:27 +0530

    Solved problem: Timeout Cancellation - 1

--- problems/Timeout_Cancellation_2025-04-20.js
+++ problems/Timeout_Cancellation_2025-04-20.js
@@ -3,4 +3,11 @@ function cancellable(fn, t) {
     // Schedule the function to run after 't' milliseconds
     const timeoutId = setTimeout(fn, t);
 
-    // Return a fu
+    // Return a function that can be used to cancel the timeout
+    return function cancel() {
+        clearTimeout(timeoutId);
+    };
+}
+
+// Example usage:
+function myF
