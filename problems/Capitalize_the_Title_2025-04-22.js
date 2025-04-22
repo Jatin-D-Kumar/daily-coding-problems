3883732077dca198a89d82d648474951c2d38ed1
Jatin Kumar <jk8826984588@gmail.com>
2025-04-22 22:00:52 +0530

    Solved problem: Capitalize the Title - 1

--- problems/Capitalize_the_Title_2025-04-22.js
+++ problems/Capitalize_the_Title_2025-04-22.js
@@ -5,4 +5,8 @@ function capitalizeTitle(title) {
     
     // Map through the words and apply capitalization rules
     const capitalizedWords = words.map((word, index) => {
- 
+        // Capitalize the first and last word or if the word has more than 2 characters
+        if (index === 0 || index === words.length - 1 || word.length > 2) {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        }
+     
