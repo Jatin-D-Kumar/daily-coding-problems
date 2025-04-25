9109dec8ffd494aec752bf3032c541e302c9270d
Jatin Kumar <jk8826984588@gmail.com>
2025-04-25 21:00:30 +0530

    Solved problem: Minimum Speed to Arrive on Time - 1

--- problems/Minimum_Speed_to_Arrive_on_Time_2025-04-25.js
+++ problems/Minimum_Speed_to_Arrive_on_Time_2025-04-25.js
@@ -3,4 +3,15 @@ function minSpeedOnTime(distances, hour) {
     const totalDistance = distances.reduce((sum, distance) => sum + distance, 0);
     
     let left = 1;  // Minimum possible speed
-    let right = Math.max(...distances) * (Math.ceil(hour));  // Maximum possible spe
+    let right = Math.max(...distances) * (Math.ceil(hour));  // Maximum possible speed
+
+    const canArriveOnTime = (speed) => {
+        let timeNeeded = 0;
+        
+        for (let i = 0; i < distances.length; i++) {
+            timeNeeded += Math.ceil(distances[i] / speed);
+        }
+        return timeNeeded <= hour;
+    };
+    
+    // Bi
