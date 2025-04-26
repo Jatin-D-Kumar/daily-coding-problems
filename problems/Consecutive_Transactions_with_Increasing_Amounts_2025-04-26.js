1a5a1a4da07859f403ff55fad40b543aa1b910e6
Jatin Kumar <jk8826984588@gmail.com>
2025-04-26 22:46:20 +0530

    Solved problem: Consecutive Transactions with Increasing Amounts - 1

--- problems/Consecutive_Transactions_with_Increasing_Amounts_2025-04-26.js
+++ problems/Consecutive_Transactions_with_Increasing_Amounts_2025-04-26.js
@@ -5,4 +5,9 @@ function findLongestIncreasingSequence(transactions) {
     }
     
     let maxLength = 1;
-   
+    let currentLength = 1;
+    let maxStartIndex = 0;
+    let currentStartIndex = 0;
+
+    for (let i = 1; i < transactions.length; i++) {
+        if (tr
