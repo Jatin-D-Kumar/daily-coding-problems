cba82a72fadbaae150ea95743f28c6173b1c05fc
Jatin Kumar <jk8826984588@gmail.com>
2025-04-27 22:44:52 +0530

    Solved problem: Find And Replace in String - 2

--- problems/Find_And_Replace_in_String_2025-04-27.js
+++ problems/Find_And_Replace_in_String_2025-04-27.js
@@ -2,3 +2,8 @@
 function findAndReplace(str, oldSubstr, newSubstr) {
   // Use the replace method with a global regular expression to replace all occurrences
   const regExp = new RegExp(oldSubstr, 'g');
+  return str.replace(regExp, newSubstr);
+}
+
+// Example usage:
+const originalString = "Hello 
