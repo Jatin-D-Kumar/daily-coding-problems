959810ad91a7964ac37611ddef3df57b089cfde9
Jatin Kumar <jk8826984588@gmail.com>
2025-04-28 22:00:54 +0530

    Solved problem: Duplicate Emails - 1

--- problems/Duplicate_Emails_2025-04-28.js
+++ problems/Duplicate_Emails_2025-04-28.js
@@ -5,4 +5,9 @@ function findDuplicateEmails(emails) {
 
     for (const email of emails) {
         // Split email into local and domain parts
-       
+        let [local, domain] = email.split('@');
+        
+        // Process the local part: remove dots and ignore characters after '+'
+        local = local.split('.').join('').split('+')[0];
+        
+        // Re
