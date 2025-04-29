b4a7645afbe2052818ab61a8869d34676df23562
Jatin Kumar <jk8826984588@gmail.com>
2025-04-29 22:00:50 +0530

    Solved problem: Minimum Number of Frogs Croaking - 1

--- problems/Minimum_Number_of_Frogs_Croaking_2025-04-29.js
+++ problems/Minimum_Number_of_Frogs_Croaking_2025-04-29.js
@@ -5,4 +5,14 @@ function minNumberOfFrogs(croakOfFrogs) {
     let frogs = 0; // current frogs croaking
     let maxFrogs = 0; // maximum frogs needed at any time
 
-    for (let char of croa
+    for (let char of croakOfFrogs) {
+        const index = croak.indexOf(char);
+
+        if (index === -1) {
+            return -1; // Invalid character
+        }
+
+        // If it's the first character 'c', we start a new frog croaking
+        if (char === 'c') {
+            frogs++;
+            maxFrog
