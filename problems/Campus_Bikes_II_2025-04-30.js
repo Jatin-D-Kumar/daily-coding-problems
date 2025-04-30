1ddcf28099dec4dae059dcabedf7c81c1d2e112a
Jatin Kumar <jk8826984588@gmail.com>
2025-04-30 22:37:15 +0530

    Solved problem: Campus Bikes II - 2

--- problems/Campus_Bikes_II_2025-04-30.js
+++ problems/Campus_Bikes_II_2025-04-30.js
@@ -12,4 +12,19 @@ const assignBikes = (workers, bikes) => {
         for (let bikeIndex = 0; bikeIndex < n; bikeIndex++) {
             if ((mask & (1 << bikeIndex)) === 0) { // If the bike is not already assigned
                 const newMask = mask | (1 << bikeIndex);
-                const distance = getDistance(workers[workerIndex], bikes[
+                const distance = getDistance(workers[workerIndex], bikes[bikeIndex]);
+                dp[newMask] = Math.min(dp[newMask], dp[mask] + distance);
+            }
+        }
+    }
+
+    return dp[(1 << n) - 1];
+};
+
+const countBits = (mask) => {
+    let count = 0;
+    while (mask > 0) {
+        count += mask & 1;
+        mask >>= 1;
+    }
+    return count
