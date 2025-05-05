ab28a38d40c827e3727a4d1becb1ffa62d0da50c
Jatin Kumar <jk8826984588@gmail.com>
2025-05-05 22:10:40 +0530

    Solved problem: Find the Child Who Has the Ball After K Seconds - 1

--- problems/Find_the_Child_Who_Has_the_Ball_After_K_Seconds_2025-05-05.js
+++ problems/Find_the_Child_Who_Has_the_Ball_After_K_Seconds_2025-05-05.js
@@ -3,4 +3,8 @@ function findBallHolder(n, start, k, direction) {
   let position = start;
 
   for (let i = 0; i < k; i++) {
-    if
+    if (direction === 1) {
+      // Clockwise
+      position = (position + 1) % n;
+    } else {
+      // Counter-clockwi
