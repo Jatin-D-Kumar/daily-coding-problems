61bd89a1551fe94badc3fa3748c4c1ceeee089b3
Jatin Kumar <jk8826984588@gmail.com>
2025-05-06 22:51:24 +0530

    Solved problem: Parallel Courses III - 1

--- problems/Parallel_Courses_III_2025-05-06.js
+++ problems/Parallel_Courses_III_2025-05-06.js
@@ -6,4 +6,14 @@ function minimumTime(n, relations, time) {
     
     // Build the graph and indegree array based on courses and relations
     for (const [a, b] of relations) {
-        graph[b - 1].pu
+        graph[b - 1].push(a - 1); // b must be finished before a
+        indegree[a - 1]++;
+    }
+    
+    // Queue for processing
+    const queue = [];
+    
+    // Initialize the minTime with the time to complete each course
+    for (let i = 0; i < n; i++) {
+        minTime[i] = time[i];
+        // Add courses with no prerequisites to the que
