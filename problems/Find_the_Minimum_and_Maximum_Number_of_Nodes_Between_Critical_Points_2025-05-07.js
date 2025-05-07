e5ffe6309939bb0b9c9bd52ef3c7f57ca8b9b86f
Jatin Kumar <jk8826984588@gmail.com>
2025-05-07 22:36:18 +0530

    Solved problem: Find the Minimum and Maximum Number of Nodes Between Critical Points - 2

--- problems/Find_the_Minimum_and_Maximum_Number_of_Nodes_Between_Critical_Points_2025-05-07.js
+++ problems/Find_the_Minimum_and_Maximum_Number_of_Nodes_Between_Critical_Points_2025-05-07.js
@@ -17,4 +17,12 @@ function findMinMaxBetweenCriticalPoints(head) {
 
     for (let current = head.next; current.next; current = current.next) {
         // Check if current node is a critical point
-        if ((prevNode.value < current.value && current.value > current.next.va
+        if ((prevNode.value < current.value && current.value > current.next.value) || 
+            (prevNode.value > current.value && current.value < current.next.value)) {
+            criticalPoints.push(position + 1); // Store the position of the critical point
+        }
+        prevNode = current;
+        position++;
+    }
+
+    if (criticalPoints.length < 2) {
