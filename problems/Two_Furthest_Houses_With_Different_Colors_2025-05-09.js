947230ce499441a7f6249692b1fc12da055deb84
Jatin Kumar <jk8826984588@gmail.com>
2025-05-09 21:33:55 +0530

    Solved problem: Two Furthest Houses With Different Colors - 1

--- problems/Two_Furthest_Houses_With_Different_Colors_2025-05-09.js
+++ problems/Two_Furthest_Houses_With_Different_Colors_2025-05-09.js
@@ -7,4 +7,17 @@ function maxDistance(houses) {
     let rightIndex = n - 1;
 
     // Find the furthest house on left with different color than the first house
-    while (leftIndex < n && houses[leftIndex] === firstHo
+    while (leftIndex < n && houses[leftIndex] === firstHouseColor) {
+        leftIndex++;
+    }
+
+    // Find the furthest house on right with different color than the last house
+    while (rightIndex >= 0 && houses[rightIndex] === lastHouseColor) {
+        rightIndex--;
+    }
+
+    let maxDist = 0;
+
+    // If there is a different color house on the left
+    if (leftIndex < n) {
+   
