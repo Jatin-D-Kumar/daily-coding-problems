debbd7e0b6847574c3b2fcaa3469991e70f786b9
Jatin Kumar <jk8826984588@gmail.com>
2025-05-11 21:07:22 +0530

    Solved problem: Minimum Interval to Include Each Query - 1

--- problems/Minimum_Interval_to_Include_Each_Query_2025-05-11.js
+++ problems/Minimum_Interval_to_Include_Each_Query_2025-05-11.js
@@ -17,4 +17,16 @@ class MinHeap {
 
     // Move the element up to restore heap property
     heapifyUp() {
-        let index = this.heap.length
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            const parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[parentIndex][1] <= this.heap[index][1]) break;
+            this.swap(parentIndex, index);
+            index = parentIndex;
+        }
+    }
+
+    // Extract the root element (minimum) from the heap
+    extractMin() {
+        if (this.heap.length === 0) return null;
+        if (this.heap.length === 1) return th
