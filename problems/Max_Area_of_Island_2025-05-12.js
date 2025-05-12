92b829f598fb552684134c193b6fa4b120aa125c
Jatin Kumar <jk8826984588@gmail.com>
2025-05-12 10:54:06 +0530

    Solved problem: Max Area of Island - 0

---
+++ problems/Max_Area_of_Island_2025-05-12.js
@@ -0,0 +1,7 @@
+
+function maxAreaOfIsland(grid) {
+    let maxArea = 0;
+
+    function dfs(i, j) {
+        // If out of bounds or water (0), return 0
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0
