f14a13870ef2c8b2ab1ab2b83e933fc41bb4809e
Jatin Kumar <jk8826984588@gmail.com>
2025-05-13 22:37:13 +0530

    Solved problem: Product Sales Analysis II - 8

--- problems/Product_Sales_Analysis_II_2025-05-13.js
+++ problems/Product_Sales_Analysis_II_2025-05-13.js
@@ -3,4 +3,6 @@ const salesData = [
   { productId: 1, productName: "Product A", quantitySold: 10, pricePerUnit: 100 },
   { productId: 2, productName: "Product B", quantitySold: 5, pricePerUnit: 150 },
   { productId: 1, productName: "Product A", quantitySold: 7, pricePerUnit: 100 },
-  { productId: 3, productName: "Product C", quantitySold: 3, pricePerUnit: 2
+  { productId: 3, productName: "Product C", quantitySold: 3, pricePerUnit: 200 },
+  // Add more sales data...
+];
