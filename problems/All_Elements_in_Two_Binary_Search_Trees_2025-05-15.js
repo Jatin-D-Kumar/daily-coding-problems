722989e12c77539f84da3b64a713ed0645d9c39c
Jatin Kumar <jk8826984588@gmail.com>
2025-05-15 21:38:36 +0530

    Solved problem: All Elements in Two Binary Search Trees - 8

--- problems/All_Elements_in_Two_Binary_Search_Trees_2025-05-15.js
+++ problems/All_Elements_in_Two_Binary_Search_Trees_2025-05-15.js
@@ -63,4 +63,9 @@ const root1 = new TreeNode(2);
 root1.left = new TreeNode(1);
 root1.right = new TreeNode(4);
 
-const root2 = new T
+const root2 = new TreeNode(1);
+root2.right = new TreeNode(3);
+
+// Getting all elements from both BSTs
+const result = getAllElements(root1, root2);
+console.log(result); // Output: [1, 1, 2, 3, 4]
