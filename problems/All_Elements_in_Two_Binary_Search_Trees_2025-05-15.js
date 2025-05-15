582e04301f63e16603a879d78720d39c3d129c0d
Jatin Kumar <jk8826984588@gmail.com>
2025-05-15 21:37:16 +0530

    Solved problem: All Elements in Two Binary Search Trees - 0

---
+++ problems/All_Elements_in_Two_Binary_Search_Trees_2025-05-15.js
@@ -0,0 +1,11 @@
+
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function getAllElements(root1, root2) {
+    const element
