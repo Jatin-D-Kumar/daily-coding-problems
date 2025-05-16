8c3a4a0f52269e7e002a2e5aaaa147fe512cff91
Jatin Kumar <jk8826984588@gmail.com>
2025-05-16 22:57:30 +0530

    Solved problem: Sum of Floored Pairs - 1

--- problems/Sum_of_Floored_Pairs_2025-05-16.js
+++ problems/Sum_of_Floored_Pairs_2025-05-16.js
@@ -5,4 +5,13 @@ function sumFlooredPairs(n) {
     for (let j = 1; j <= max_j; j++) {
         const jBigInt = BigInt(j);
         const m = BigInt(n) / jBigInt;
-        const k_max =
+        const k_max = m / jBigInt;
+        const numerator = m - (jBigInt - 1n);
+        let k0;
+        if (numerator < 0n) {
+            k0 = 0n;
+        } else {
+            k0 = numerator / jBigInt;
+        }
+        let sum1 = 0n;
+        if (k0
