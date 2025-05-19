f9a0319666ea43f670a86d10a42e322e2a76aa0b
Jatin Kumar <jk8826984588@gmail.com>
2025-05-19 21:46:45 +0530

    Solved problem: Meeting Rooms - 4

--- problems/Meeting_Rooms_2025-05-19.js
+++ problems/Meeting_Rooms_2025-05-19.js
@@ -10,4 +10,7 @@ function canAttendMeetings(intervals) {
 
     // Check for overlaps
     for (let i = 1; i < intervals.length; i++) {
-        // If the start time of the current meeting is less than the end time of the previous meeting
+        // If the start time of the current meeting is less than the end time of the previous meeting, there is an overlap
+        if (intervals[i][0] < intervals[i - 1][1]) {
+            return false;
+ 
