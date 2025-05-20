0e00202e989ca90d764568587df91b42e38a7ef2
Jatin Kumar <jk8826984588@gmail.com>
2025-05-20 23:14:35 +0530

    Solved problem: Select Data - 1

--- problems/Select_Data_2025-05-20.js
+++ problems/Select_Data_2025-05-20.js
@@ -1,4 +1,5 @@
 
 // Sample data: Array of user objects
 const users = [
-    { id: 1, name: 'Al
+    { id: 1, name: 'Alice', age: 25, gender: 'female' },
+    { id: 2, name: 'Bob', age: 30, gender:
