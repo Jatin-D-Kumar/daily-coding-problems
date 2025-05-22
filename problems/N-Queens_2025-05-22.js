41369a1c8ae6b477b368114d1f119b5d805c12af
Jatin Kumar <jk8826984588@gmail.com>
2025-05-22 23:00:38 +0530

    Solved problem: N-Queens - 2

--- problems/N-Queens_2025-05-22.js
+++ problems/N-Queens_2025-05-22.js
@@ -6,4 +6,8 @@ function solveNQueens(n) {
     const isSafe = (row, col) => {
         // Check the column
         for (let i = 0; i < row; i++) {
-            if (board[i][col] === 'Q') return f
+            if (board[i][col] === 'Q') return false;
+        }
+        // Check the upper left diagonal
+        for (let i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if 
