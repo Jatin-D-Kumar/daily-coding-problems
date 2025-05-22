856013a0955034bbd8c6547481e6bc4682c02bae
Jatin Kumar <jk8826984588@gmail.com>
2025-05-22 23:00:28 +0530

    Solved problem: N-Queens - 1

--- problems/N-Queens_2025-05-22.js
+++ problems/N-Queens_2025-05-22.js
@@ -3,4 +3,7 @@ function solveNQueens(n) {
     const results = [];
     const board = Array.from({ length: n }, () => Array(n).fill('.'));
 
-    const isS
+    const isSafe = (row, col) => {
+        // Check the column
+        for (let i = 0; i < row; i++) {
+            if (board[i][col] === 'Q') return f
