4c206595df4e99acc0b781e7cb0a6ca9e2c43394
Jatin Kumar <jk8826984588@gmail.com>
2025-05-23 22:19:47 +0530

    Solved problem: Rearrange Array Elements by Sign - 1

--- problems/Rearrange_Array_Elements_by_Sign_2025-05-23.js
+++ problems/Rearrange_Array_Elements_by_Sign_2025-05-23.js
@@ -3,4 +3,7 @@ function rearrangeArray(arr) {
     const positiveArray = [];
     const negativeArray = [];
 
-    // Loop through the a
+    // Loop through the array and separate positive and negative numbers
+    for (let num of arr) {
+        if (num >= 0) {
+            positiv
