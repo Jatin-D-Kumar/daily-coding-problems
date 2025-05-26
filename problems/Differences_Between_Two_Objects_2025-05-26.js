0b3a41430cfb5834dc18c2adf03eb16837ff6fa0
Jatin Kumar <jk8826984588@gmail.com>
2025-05-26 21:29:31 +0530

    Solved problem: Differences Between Two Objects - 1

--- problems/Differences_Between_Two_Objects_2025-05-26.js
+++ problems/Differences_Between_Two_Objects_2025-05-26.js
@@ -3,4 +3,7 @@ function getObjectDifferences(obj1, obj2) {
     const differences = {};
 
     // Check properties in obj1
-    for (const key
+    for (const key in obj1) {
+        if (obj1.hasOwnProperty(key)) {
+            if (!obj2.hasOwnProperty(key)) {
+                // Property
