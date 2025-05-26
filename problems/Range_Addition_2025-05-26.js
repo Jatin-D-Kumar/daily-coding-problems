15172a8c71b5c1d53e40b1b572000d2aa4c61beb
Jatin Kumar <jk8826984588@gmail.com>
2025-05-26 21:56:23 +0530

    Solved problem: Range Addition - 2

--- problems/Range_Addition_2025-05-26.js
+++ problems/Range_Addition_2025-05-26.js
@@ -3,4 +3,5 @@ function getModifiedArray(length, updates) {
     const nums = new Array(length).fill(0); // Initialize the array with zeros
 
     // Apply the range update logic using the difference array concept
-    f
+    for (const [startIndex, endIndex, increment] of updates) {
+        nums[startIndex] += increment; // S
