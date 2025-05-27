0502524738221aaedf69f132830eb0feccf34c49
Jatin Kumar <jk8826984588@gmail.com>
2025-05-27 22:19:07 +0530

    Solved problem: Process Tasks Using Servers - 1

--- problems/Process_Tasks_Using_Servers_2025-05-27.js
+++ problems/Process_Tasks_Using_Servers_2025-05-27.js
@@ -1,4 +1,8 @@
 
 function processTasks(servers, tasks) {
     // Create a priority queue for available servers
-    let serverQueue = [...servers].sort((a, b) => a - b
+    let serverQueue = [...servers].sort((a, b) => a - b);
+    let taskCompletion = new Array(tasks.length).fill(0);
+    
+    // Timers for each server
+    let serverTimers = Array(servers.length).fill(0);
