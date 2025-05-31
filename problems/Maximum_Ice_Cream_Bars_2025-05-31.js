9164204b9ab01fe73504b9212ad2f9de0a01f360
Jatin Kumar <jk8826984588@gmail.com>
2025-05-31 22:33:04 +0530

    Solved problem: Maximum Ice Cream Bars - 1

--- problems/Maximum_Ice_Cream_Bars_2025-05-31.js
+++ problems/Maximum_Ice_Cream_Bars_2025-05-31.js
@@ -1,4 +1,7 @@
 
 function maxIceCream(costs, coins) {
     // Sort the costs in ascending order
-    costs.sort((a
+    costs.sort((a, b) => a - b);
+    
+    let count = 0; // To count the number of ice cream bars
+    for (let co
