436b1284382def2b5b3ef61074a692ac5fdce066
Jatin Kumar <jk8826984588@gmail.com>
2025-06-03 21:42:42 +0530

    Solved problem: Minimum Changes To Make Alternating Binary String - 1

--- problems/Minimum_Changes_To_Make_Alternating_Binary_String_2025-06-03.js
+++ problems/Minimum_Changes_To_Make_Alternating_Binary_String_2025-06-03.js
@@ -1,4 +1,7 @@
 
 function minChangesToMakeAlternatingBinaryString(s) {
     let changesForPattern1 = 0; // For pattern starting with '0'
-    let changesForPatte
+    let changesForPattern2 = 0; // For pattern starting with '1'
+
+    for (let i = 0; i < s.length; i++) {
+        // Determine what the expected character is for bot
