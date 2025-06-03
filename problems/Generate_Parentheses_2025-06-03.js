08a248a3571995130410b0cc8818faf236679235
Jatin Kumar <jk8826984588@gmail.com>
2025-06-03 20:08:15 +0530

    Solved problem: Generate Parentheses - 1

--- problems/Generate_Parentheses_2025-06-03.js
+++ problems/Generate_Parentheses_2025-06-03.js
@@ -3,4 +3,5 @@ function generateParenthesis(n) {
     const result = [];
 
     function backtrack(current, open, close) {
-   
+        // If the current string is of the maximum length, add it to the result
+        if (current.length === n
