a60c67050ed72621a671854686cea46e51a3dc09
Jatin Kumar <jk8826984588@gmail.com>
2025-06-05 21:00:26 +0530

    Solved problem: Process Tasks Using Servers - 0

---
+++ problems/Process_Tasks_Using_Servers_2025-06-05.js
@@ -0,0 +1,17 @@
+
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        const element = this.heap[index];
+
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) 
