587f5080ad1ca57bf3d03ee92e3047d198c79a4d
Jatin Kumar <jk8826984588@gmail.com>
2025-06-06 21:01:02 +0530

    Solved problem: Sum of Subsequence Widths - 4

--- problems/Sum_of_Subsequence_Widths_2025-06-06.js
+++ problems/Sum_of_Subsequence_Widths_2025-06-06.js
@@ -9,4 +9,8 @@ function sumSubseqWidths(A) {
 
     // Precompute powers of 2 up to n
     const pow2 = new Array(n).fill(1);
-    for (let i = 1; i < 
+    for (let i = 1; i < n; i++) {
+        pow2[i] = (pow2[i - 1] * 2) % mod;
+    }
+
+    // Calculate
