62c56af5a960b267e36e8b2111ff384fc22399b0
Jatin Kumar <jk8826984588@gmail.com>
2025-06-07 22:18:54 +0530

    Solved problem: Solving Questions With Brainpower - 2

--- problems/Solving_Questions_With_Brainpower_2025-06-07.js
+++ problems/Solving_Questions_With_Brainpower_2025-06-07.js
@@ -4,4 +4,5 @@ function maxQuestions(questions) {
     const dp = new Array(n + 1).fill(0);
 
     for (let i = 0; i < n; i++) {
-        const [points,
+        const [points, brainpower] = questions[i];
+        // Iterate backwards to avoid overwriting the 
