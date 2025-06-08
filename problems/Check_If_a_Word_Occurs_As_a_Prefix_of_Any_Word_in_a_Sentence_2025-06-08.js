b07f331165ea509c755a44836e35b9031aadc80e
Jatin Kumar <jk8826984588@gmail.com>
2025-06-08 20:53:09 +0530

    Solved problem: Check If a Word Occurs As a Prefix of Any Word in a Sentence - 2

--- problems/Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence_2025-06-08.js
+++ problems/Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence_2025-06-08.js
@@ -3,4 +3,6 @@ function isPrefixOfWord(sentence, searchWord) {
     // Split the sentence into words
     const words = sentence.split(' ');
 
-    // Iterate through eac
+    // Iterate through each word in the sentence
+    for (let i = 0; i < words.length; i++) {
+        
