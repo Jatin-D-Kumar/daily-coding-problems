2ecc0541889c81ecb2e72b57b1894d25747d922b
Jatin Kumar <jk8826984588@gmail.com>
2025-06-11 21:39:31 +0530

    Solved problem: Count Good Triplets - 2

--- problems/Count_Good_Triplets_2025-06-11.js
+++ problems/Count_Good_Triplets_2025-06-11.js
@@ -6,4 +6,6 @@ function countGoodTriplets(arr, a, b, c) {
     for (let i = 0; i < n; i++) {
         for (let j = i + 1; j < n; j++) {
             for (let k = j + 1; k < n; k++) {
-  
+                if (Math.abs(arr[i] - arr[j]) <= a &&
+                    Math.abs(arr[j] - arr[k]) <= b &&
+ 
