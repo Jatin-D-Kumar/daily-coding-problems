7d0cabd7bc46a9d636f2d7e7e1d67e3bd3b69b01
Jatin Kumar <jk8826984588@gmail.com>
2025-06-13 20:11:55 +0530

    Solved problem: Maximum Score From Removing Stones - 2

--- problems/Maximum_Score_From_Removing_Stones_2025-06-13.js
+++ problems/Maximum_Score_From_Removing_Stones_2025-06-13.js
@@ -3,4 +3,6 @@ function maxScore(stones) {
     // Calculate the total number of stones
     let totalStones = stones.reduce((acc, curr) => acc + curr, 0);
     
-    // The maximum possible score we can 
+    // The maximum possible score we can achieve:
+    // Each operation removes stones worth either 1 or 2 points. 
+    // Thus, the s
