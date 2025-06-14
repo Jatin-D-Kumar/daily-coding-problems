fbc1539cc7977ce5279ad864bfb039332861dc7b
Jatin Kumar <jk8826984588@gmail.com>
2025-06-14 22:45:29 +0530

    Solved problem: Count and Say - 1

--- problems/Count_and_Say_2025-06-14.js
+++ problems/Count_and_Say_2025-06-14.js
@@ -4,4 +4,8 @@ function countAndSay(n) {
     if (n === 1) return "1";
 
     // Get the previous term
-    let previo
+    let previousTerm = countAndSay(n - 1);
+    let result = '';
+    let count = 1;
+
+    // Loop through the previous term to build 
