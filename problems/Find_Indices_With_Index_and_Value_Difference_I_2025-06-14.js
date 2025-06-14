d0bd05bba36531b84f029892db238b7a857cbaec
Jatin Kumar <jk8826984588@gmail.com>
2025-06-14 13:35:57 +0530

    Solved problem: Find Indices With Index and Value Difference I - 2

--- problems/Find_Indices_With_Index_and_Value_Difference_I_2025-06-14.js
+++ problems/Find_Indices_With_Index_and_Value_Difference_I_2025-06-14.js
@@ -2,4 +2,6 @@
 function findIndicesWithDifference(arr, k) {
     const result = [];
     
-    for (let i = 0; i < arr.length; 
+    for (let i = 0; i < arr.length; i++) {
+        if (Math.abs(i - arr[i]) === k) {
+      
