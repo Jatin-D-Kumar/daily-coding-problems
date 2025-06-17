6fd53465543b6fa95dd6d0bc4682a9f2cb613e99
Jatin Kumar <jk8826984588@gmail.com>
2025-06-17 22:11:16 +0530

    Solved problem: Binary Watch - 1

--- problems/Binary_Watch_2025-06-17.js
+++ problems/Binary_Watch_2025-06-17.js
@@ -3,4 +3,6 @@ function readBinaryWatch(turnedOn) {
     const result = [];
     
     for (let h = 0; h < 12; h++) {
-        for (le
+        for (let m = 0; m < 60; m++) {
+            // Count the number of 1s in the binary representation of hours and minutes
+    
