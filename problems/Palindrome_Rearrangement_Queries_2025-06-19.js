eb998e7d17babe418b0f1f6642f0b4b0dc519bdf
Jatin Kumar <jk8826984588@gmail.com>
2025-06-19 21:00:31 +0530

    Solved problem: Palindrome Rearrangement Queries - 1

--- problems/Palindrome_Rearrangement_Queries_2025-06-19.js
+++ problems/Palindrome_Rearrangement_Queries_2025-06-19.js
@@ -2,4 +2,9 @@
 function canFormPalindrome(s) {
     const charCount = {};
     
-    // Count the occurrences of each cha
+    // Count the occurrences of each character
+    for (let char of s) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+    
+    le
