e9c89155ebfe1db8e2b3f4a1a401940cbf67b1ad
Jatin Kumar <jk8826984588@gmail.com>
2025-06-20 22:04:47 +0530

    Solved problem: Make Array Strictly Increasing - 1

--- problems/Make_Array_Strictly_Increasing_2025-06-20.js
+++ problems/Make_Array_Strictly_Increasing_2025-06-20.js
@@ -3,4 +3,9 @@ function canBeMadeStrictlyIncreasing(arr) {
     let count = 0; // Counter for the number of modifications
 
     for (let i = 1; i < arr.length; i++) {
-        // If the current element is not greate
+        // If the current element is not greater than the previous one
+        if (arr[i] <= arr[i - 1]) {
+            count++;
+            // If we've already modified an element, return false
+            if (count > 1) {
+                return
