d971161e99a8b8ada51953bfc9fcdf5388a97528
Jatin Kumar <jk8826984588@gmail.com>
2025-06-21 21:00:25 +0530

    Solved problem: Make the Prefix Sum Non-negative - 1

--- problems/Make_the_Prefix_Sum_Non-negative_2025-06-21.js
+++ problems/Make_the_Prefix_Sum_Non-negative_2025-06-21.js
@@ -4,4 +4,8 @@ function minInitialValue(nums) {
     let currentPrefixSum = 0;
 
     for (let num of nums) {
-   
+        currentPrefixSum += num;
+        minPrefixSum = Math.min(minPrefixSum, currentPrefixSum);
+    }
+
+    // If minPrefixS
