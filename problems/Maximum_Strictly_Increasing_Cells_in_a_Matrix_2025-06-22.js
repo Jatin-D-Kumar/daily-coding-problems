ebb6907c5d613e3ef73f5914f9d1cb09fc94c8df
Jatin Kumar <jk8826984588@gmail.com>
2025-06-22 23:26:42 +0530

    Solved problem: Maximum Strictly Increasing Cells in a Matrix - 1

--- problems/Maximum_Strictly_Increasing_Cells_in_a_Matrix_2025-06-22.js
+++ problems/Maximum_Strictly_Increasing_Cells_in_a_Matrix_2025-06-22.js
@@ -2,4 +2,10 @@
 function maxIncreasingCells(matrix) {
     const rows = matrix.length;
     const cols = matrix[0].length;
-    const dp = Array.from({ length:
+    const dp = Array.from({ length: rows }, () => Array(cols).fill(-1));
+    let maxLength = 0;
+
+    const directions = [
+        [0, 1], // right
+        [1, 0], // down
+     
