eab67eb7cc47652d1a80a99023808e214958c593
Jatin Kumar <jk8826984588@gmail.com>
2025-06-24 23:03:19 +0530

    Solved problem: Maximize the Number of Partitions After Operations - 1

--- problems/Maximize_the_Number_of_Partitions_After_Operations_2025-06-24.js
+++ problems/Maximize_the_Number_of_Partitions_After_Operations_2025-06-24.js
@@ -3,4 +3,5 @@ function maximizePartitions(arr, maxValue) {
     let partitions = 0;
     let currentSum = 0;
 
-    for (l
+    for (let i = 0; i < arr.length; i++) {
+        // Check if we can include the current element in the current pa
