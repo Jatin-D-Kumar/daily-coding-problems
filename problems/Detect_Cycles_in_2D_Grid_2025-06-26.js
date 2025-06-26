8e57d6ec416b90dc93ec25bd13b19ca4d9773d00
Jatin Kumar <jk8826984588@gmail.com>
2025-06-26 21:51:38 +0530

    Solved problem: Detect Cycles in 2D Grid - 1

--- problems/Detect_Cycles_in_2D_Grid_2025-06-26.js
+++ problems/Detect_Cycles_in_2D_Grid_2025-06-26.js
@@ -2,4 +2,13 @@
 function containsCycle(grid) {
     const rows = grid.length;
     const cols = grid[0].length;
-    const visited = Array.from({ length: rows }, () => Array(cols).fill(false)
+    const visited = Array.from({ length: rows }, () => Array(cols).fill(false));
+
+    const directions = [
+        [1, 0], // down
+        [-1, 0], // up
+        [0, 1], // right
+        [0, -1] // left
+    ];
+
+    function isValid(x, y, parentX, pare
