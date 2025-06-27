0fcf166c9aaa5d3c2e3b16519302fd9adc442072
Jatin Kumar <jk8826984588@gmail.com>
2025-06-27 22:00:04 +0530

    Solved problem: Binary Tree Level Order Traversal - 0

---
+++ problems/Binary_Tree_Level_Order_Traversal_2025-06-27.js
@@ -0,0 +1,11 @@
+
+class TreeNode {
+    constructor(val = 0, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+function levelOrder(root) {
+    if (!root) return [];
