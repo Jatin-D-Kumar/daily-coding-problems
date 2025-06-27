eb5b9381044efb79dd1b3b37298f17a019012d31
Jatin Kumar <jk8826984588@gmail.com>
2025-06-27 22:01:03 +0530

    Solved problem: Binary Tree Level Order Traversal - 5

--- problems/Binary_Tree_Level_Order_Traversal_2025-06-27.js
+++ problems/Binary_Tree_Level_Order_Traversal_2025-06-27.js
@@ -34,4 +34,10 @@ function levelOrder(root) {
 }
 
 // Example usage:
-const root =
+const root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+console.log(levelOrder(root)); // Output: [[1], [2, 3], [4, 5]]
