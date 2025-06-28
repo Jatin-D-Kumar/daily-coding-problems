53f6ebdf84ee3ea6e98cf005b273e752613c5594
Jatin Kumar <jk8826984588@gmail.com>
2025-06-28 18:15:42 +0530

    Solved problem: Integer Break - 1

--- problems/Integer_Break_2025-06-28.js
+++ problems/Integer_Break_2025-06-28.js
@@ -1,3 +1,5 @@
 
 function integerBreak(n) {
-    // Create an array to store the maximum product for values 1 t
+    // Create an array to store the maximum product for values 1 to n
+    const dp = new Array(n + 1).fill(0);
+    dp[1] = 1; // Base case: the maximum product 
