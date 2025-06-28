5f58e45058f1b46251e7244f8d60f09c380620fa
Jatin Kumar <jk8826984588@gmail.com>
2025-06-28 22:26:26 +0530

    Solved problem: Build Array Where You Can Find The Maximum Exactly K Comparisons - 1

--- problems/Build_Array_Where_You_Can_Find_The_Maximum_Exactly_K_Comparisons_2025-06-28.js
+++ problems/Build_Array_Where_You_Can_Find_The_Maximum_Exactly_K_Comparisons_2025-06-28.js
@@ -1,4 +1,11 @@
 
 function buildArray(n, m, k) {
     // If K is larger than n, we can fill the array with the maximum value
-    if (k >= 
+    if (k >= n) {
+        return Array(n).fill(m);
+    }
+
+    // The result array
+    const result = new Array(n).fill(0);
+    
+    /
