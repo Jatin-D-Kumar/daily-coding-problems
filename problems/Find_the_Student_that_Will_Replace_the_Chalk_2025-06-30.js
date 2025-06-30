a563b0748b5f609c029afcf63f1155099a0412af
Jatin Kumar <jk8826984588@gmail.com>
2025-06-30 20:25:52 +0530

    Solved problem: Find the Student that Will Replace the Chalk - 2

--- problems/Find_the_Student_that_Will_Replace_the_Chalk_2025-06-30.js
+++ problems/Find_the_Student_that_Will_Replace_the_Chalk_2025-06-30.js
@@ -8,4 +8,8 @@ function findStudentToReplaceChalk(chalk, dailyUsage) {
     const chalkUsedInFullCycles = fullCycles * dailyUsage;
     
     // Remaining chalk after full cycles
-    let remainingChalk = tot
+    let remainingChalk = totalChalk - chalkUsedInFullCycles;
+
+    // Determine which student will run out of chalk first
+    for (let i = 0; i < totalStudents; i++) {
+        // If the remaining chalk can cover the current student's usage
