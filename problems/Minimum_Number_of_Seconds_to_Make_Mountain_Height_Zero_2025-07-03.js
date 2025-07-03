f3fb40578465f53cc461844829fedf65fc5c99dc
Jatin Kumar <jk8826984588@gmail.com>
2025-07-03 21:00:59 +0530

    Solved problem: Minimum Number of Seconds to Make Mountain Height Zero - 3

--- problems/Minimum_Number_of_Seconds_to_Make_Mountain_Height_Zero_2025-07-03.js
+++ problems/Minimum_Number_of_Seconds_to_Make_Mountain_Height_Zero_2025-07-03.js
@@ -3,4 +3,9 @@ function minimumSecondsToMakeMountainHeightZero(mountains) {
     // Find the maximum height from the mountains
     let maxHeight = Math.max(...mountains);
     // Return the maximum height as it represents the required seconds
- 
+    return maxHeight;
+}
+
+// Example usage:
+const mountains = [3, 2, 1, 4];
+co
