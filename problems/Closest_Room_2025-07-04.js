fae9c9fbc25525712a0622889e539ee69f76c171
Jatin Kumar <jk8826984588@gmail.com>
2025-07-04 22:22:46 +0530

    Solved problem: Closest Room - 1

--- problems/Closest_Room_2025-07-04.js
+++ problems/Closest_Room_2025-07-04.js
@@ -1,4 +1,5 @@
 
 function closestRoom(rooms, queries) {
     const results = new Array(queries.length).fill(-1);
-    const sortedQueries = quer
+    const sortedQueries = queries.map((q, index) => ({ ...q, index })).sort((a, b) => b.minArea - a.minArea);
+    const sortedRooms = rooms.map((room, index
