d4a21294e0b4817ad73adff235bcb5db1c0746e9
Jatin Kumar <jk8826984588@gmail.com>
2025-07-05 22:34:24 +0530

    Solved problem: Find the Maximum Length of Valid Subsequence I - 2

--- problems/Find_the_Maximum_Length_of_Valid_Subsequence_I_2025-07-05.js
+++ problems/Find_the_Maximum_Length_of_Valid_Subsequence_I_2025-07-05.js
@@ -5,4 +5,6 @@ function lengthOfLIS(nums) {
     // Create an array to store the length of the longest increasing subsequence
     const dp = new Array(nums.length).fill(1);
 
-   
+    // Iterate through the array
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j 
