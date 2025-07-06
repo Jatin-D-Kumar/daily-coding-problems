fa010a81a67af538b63b693135b6e851e4fd1d6f
Jatin Kumar <jk8826984588@gmail.com>
2025-07-06 23:07:57 +0530

    Solved problem: Best Time to Buy and Sell Stock IV - 1

--- problems/Best_Time_to_Buy_and_Sell_Stock_IV_2025-07-06.js
+++ problems/Best_Time_to_Buy_and_Sell_Stock_IV_2025-07-06.js
@@ -4,4 +4,11 @@ function maxProfit(k, prices) {
     if (n === 0 || k === 0) return 0;
 
     // If k is greater than half the number of days, we can make as many transactions as we want
-    if (k >= n /
+    if (k >= n / 2) {
+        let maxProfit = 0;
+        for (let i = 1; i < n; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return max
