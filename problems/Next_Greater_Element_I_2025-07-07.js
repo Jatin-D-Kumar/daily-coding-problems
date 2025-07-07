f41db2b2870f5a0b8901c31fa46a7e9cfd925210
Jatin Kumar <jk8826984588@gmail.com>
2025-07-07 21:29:50 +0530

    Solved problem: Next Greater Element I - 1

--- problems/Next_Greater_Element_I_2025-07-07.js
+++ problems/Next_Greater_Element_I_2025-07-07.js
@@ -2,3 +2,7 @@
 function nextGreaterElement(nums1, nums2) {
     const result = new Map();
     const stack = [];
+
+    // Traverse nums2 to find the next greater elements
+    for (let num of nums2) {
+        w
