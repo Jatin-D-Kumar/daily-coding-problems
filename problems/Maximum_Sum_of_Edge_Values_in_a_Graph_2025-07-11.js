4beac93cc8a975c4f0da5c1143161b7b4294c3cd
Jatin Kumar <jk8826984588@gmail.com>
2025-07-11 23:11:09 +0530

    Solved problem: Maximum Sum of Edge Values in a Graph - 1

--- problems/Maximum_Sum_of_Edge_Values_in_a_Graph_2025-07-11.js
+++ problems/Maximum_Sum_of_Edge_Values_in_a_Graph_2025-07-11.js
@@ -6,4 +6,10 @@ class Graph {
 
     addVertex(vertex) {
         if (!this.adjacencyList[vertex]) {
-            this.adjacencyList[vertex
+            this.adjacencyList[vertex] = [];
+        }
+    }
+
+    addEdge(vertex1, vertex2, value) {
+        this.adjacencyList[vertex1].push({ node: vertex2, value });
+        this.adjacencyList[vertex2].push({ node
