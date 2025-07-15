9f4775d1aebbd21b657d8f5eef4574763925d78a
Jatin Kumar <jk8826984588@gmail.com>
2025-07-15 21:00:39 +0530

    Solved problem: Parsing A Boolean Expression - 1

--- problems/Parsing_A_Boolean_Expression_2025-07-15.js
+++ problems/Parsing_A_Boolean_Expression_2025-07-15.js
@@ -6,4 +6,9 @@ function evalBooleanExpr(expr) {
     const precedence = { 'AND': 2, 'OR': 1, 'NOT': 3 };
 
     for (const token of tokens) {
-        if (isBoolean(token)
+        if (isBoolean(token)) {
+            outputQueue.push(token);
+        } else if (token in precedence) {
+            while (operatorStack.length && 
+                   precedence[operatorStack[operatorStack.length - 1]] >= precedence[token]) {
+                outputQu
