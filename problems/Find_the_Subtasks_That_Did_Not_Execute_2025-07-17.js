7e6b8eb74a5d6e40bc1bc8c67d57307bee982155
Jatin Kumar <jk8826984588@gmail.com>
2025-07-17 22:12:37 +0530

    Solved problem: Find the Subtasks That Did Not Execute - 2

--- problems/Find_the_Subtasks_That_Did_Not_Execute_2025-07-17.js
+++ problems/Find_the_Subtasks_That_Did_Not_Execute_2025-07-17.js
@@ -3,4 +3,7 @@ function findMissingSubtasks(totalSubtasks, executedSubtasks) {
     // Create a Set from the executed subtasks for O(1) lookup
     const executedSet = new Set(executedSubtasks);
     
-    // Filter the total subtasks to find those not
+    // Filter the total subtasks to find those not in the executed set
+    const missingSubtasks = totalSubtasks.filter(subtask => !executedSet.has(subtask));
+    
+   
