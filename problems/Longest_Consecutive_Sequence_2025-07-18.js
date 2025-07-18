5af046f9bda866078d288d86cce2625f6d5b29fa
JatinKumar@29 <jk8826984588@gmail.com>
2025-07-18 13:56:32 +0530

    Solved Problem: Longest Consecutive Sequence - 2

--- problems/Longest_Consecutive_Sequence_2025-07-18.js
+++ problems/Longest_Consecutive_Sequence_2025-07-18.js
@@ -1,3 +1,6 @@
 function longestConsecutive(nums) {
 const numSet = new Set(nums);
     let longest = 0;
+for (let num of numSet) {
+        // Check only starting points of sequences
+        if (!numSet.has(num - 1)) {
