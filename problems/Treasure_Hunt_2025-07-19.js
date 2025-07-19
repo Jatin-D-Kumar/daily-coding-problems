45811e4db796ba0a0532762b241b46d51e6a318b
JatinKumar@29 <jk8826984588@gmail.com>
2025-07-19 22:27:19 +0530

    Solved Problem: Treasure Hunt - 1

--- problems/Treasure_Hunt_2025-07-19.js
+++ problems/Treasure_Hunt_2025-07-19.js
@@ -1,3 +1,8 @@
 function countUniqueCluePairs(clues, target) {
   const freq = new Map();
   let count = 0;
+  
+// Step 1: Build frequency map
+  for (const num of clues) {
+    freq.set(num, (freq.get(num) || 0) + 1);
+  }
