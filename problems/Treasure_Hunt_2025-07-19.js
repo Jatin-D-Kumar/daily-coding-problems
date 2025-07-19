d70578f94859f97828238163dd803600ba3a9b4d
JatinKumar@29 <jk8826984588@gmail.com>
2025-07-19 22:28:26 +0530

    Solved Problem: Treasure Hunt - 2

--- problems/Treasure_Hunt_2025-07-19.js
+++ problems/Treasure_Hunt_2025-07-19.js
@@ -6,3 +6,7 @@ function countUniqueCluePairs(clues, target) {
   for (const num of clues) {
     freq.set(num, (freq.get(num) || 0) + 1);
   }
+
+// Step 2: Iterate through unique keys
+  for (const [num, val] of freq.entries()) {
+    const complement = target - num;
