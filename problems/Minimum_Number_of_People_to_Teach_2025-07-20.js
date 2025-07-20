05609e6bd30e3291b4e1a185ce49322de251b923
Jatin Kumar <jk8826984588@gmail.com>
2025-07-20 18:50:43 +0530

    Solved problem: Minimum Number of People to Teach - 1

--- problems/Minimum_Number_of_People_to_Teach_2025-07-20.js
+++ problems/Minimum_Number_of_People_to_Teach_2025-07-20.js
@@ -2,4 +2,7 @@
 function minPeopleToTeach(lang, knowledge) {
     let knownLanguages = new Set();
     
-    // Gather all known lan
+    // Gather all known languages from the people
+    for (const langs of knowledge) {
+        for (const l of langs) {
+            knownLang
