f4e25c7540eaae48dccdea5cd859c80d7e3f176b
Jatin Kumar <jk8826984588@gmail.com>
2025-07-20 22:10:03 +0530

    Solved problem: Add Strings - 1

--- problems/Add_Strings_2025-07-20.js
+++ problems/Add_Strings_2025-07-20.js
@@ -5,4 +5,6 @@ function addStrings(num1, num2) {
     let i = num1.length - 1;
     let j = num2.length - 1;
 
-    // Loop unti
+    // Loop until both numbers are fully processed
+    while (i >= 0 || j >= 0 || carry > 0) {
+        let x = i >= 0 ? num1[i] - '0' : 0; // Convert char to int if i
