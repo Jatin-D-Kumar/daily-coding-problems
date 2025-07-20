6885c20f2bdd6f414041722a035a2fbe1a4529f6
Jatin Kumar <jk8826984588@gmail.com>
2025-07-20 22:08:49 +0530

    Solved problem: Encode Number - 1

--- problems/Encode_Number_2025-07-20.js
+++ problems/Encode_Number_2025-07-20.js
@@ -1,3 +1,7 @@
 
 function encodeNumber(num) {
     if (num < 1) return ""; // Handle case where number is less than 1
+
+    let result = '';
+    while (num > 0) {
+        // Adjust num because 'a' should map to 1, not 
