8a0cbce4769777a24b7950208a191178b12c976a
Jatin Kumar <jk8826984588@gmail.com>
2025-07-26 21:40:38 +0530

    Solved problem: Furthest Point From Origin - 1

--- problems/Furthest_Point_From_Origin_2025-07-26.js
+++ problems/Furthest_Point_From_Origin_2025-07-26.js
@@ -1,4 +1,7 @@
 
 function furthestPoint(points) {
     let maxDistance = -1; // Initialize max distance
-    let fu
+    let furthestPoint = null; // To store the furthest point
+
+    for (let point of points) {
+        // Ca
