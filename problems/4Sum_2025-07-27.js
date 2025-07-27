e57fa702be0f54e5f5cea1d19db2c79f9fb85c7b
Jatin Kumar <jk8826984588@gmail.com>
2025-07-27 23:03:06 +0530

    Solved problem: 4Sum - 1

--- problems/4Sum_2025-07-27.js
+++ problems/4Sum_2025-07-27.js
@@ -7,4 +7,12 @@ function fourSum(nums, target) {
 
     for (let i = 0; i < n - 3; i++) {
         // Skip duplicate values for the first element
-        if (i > 0 && nums[i] === nums[i -
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        for (let j = i + 1; j < n - 2; j++) {
+            // Skip duplicate values for the second element
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+
+            let left = j + 1;
+            let right = n - 1;
+    
