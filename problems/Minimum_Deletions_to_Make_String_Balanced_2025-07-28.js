22c4c40c6bb7021902f176efe0fd3ad2895a582b
Jatin Kumar <jk8826984588@gmail.com>
2025-07-28 19:42:51 +0530

    Solved problem: Minimum Deletions to Make String Balanced - 2

--- problems/Minimum_Deletions_to_Make_String_Balanced_2025-07-28.js
+++ problems/Minimum_Deletions_to_Make_String_Balanced_2025-07-28.js
@@ -3,4 +3,6 @@ function minDeletionsToMakeBalanced(s) {
     let countA = 0;
     let countB = 0;
 
-    // Count the occurrences of 'a'
+    // Count the occurrences of 'a' and 'b'
+    for (let char of s) {
+        if (char === 'a'
