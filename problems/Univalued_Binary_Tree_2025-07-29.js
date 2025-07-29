dcd397e144744b52de695f9f02bcb7d8c3b9770e
Jatin Kumar <jk8826984588@gmail.com>
2025-07-29 22:26:17 +0530

    Solved problem: Univalued Binary Tree - 1

--- problems/Univalued_Binary_Tree_2025-07-29.js
+++ problems/Univalued_Binary_Tree_2025-07-29.js
@@ -3,4 +3,9 @@ class TreeNode {
     constructor(val) {
         this.val = val;
         this.left = null;
-        this
+        this.right = null;
+    }
+}
+
+function isUnivalTree(root) {
+    if (!root) return true; // An empty tree is c
