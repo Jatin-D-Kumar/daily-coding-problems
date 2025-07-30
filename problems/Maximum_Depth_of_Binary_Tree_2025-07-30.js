9bb964f950bc042cf860179878401f60521f56e7
Jatin Kumar <jk8826984588@gmail.com>
2025-07-30 22:40:24 +0530

    Solved problem: Maximum Depth of Binary Tree - 1

--- problems/Maximum_Depth_of_Binary_Tree_2025-07-30.js
+++ problems/Maximum_Depth_of_Binary_Tree_2025-07-30.js
@@ -4,3 +4,11 @@ class TreeNode {
     constructor(val = 0, left = null, right = null) {
         this.val = val;
         this.left = left;
+        this.right = right;
+    }
+}
+
+function maxDepth(root) {
+    // Base case: if the tree is empty, the depth is 0
+    if (root === null) {
+        return 0;
