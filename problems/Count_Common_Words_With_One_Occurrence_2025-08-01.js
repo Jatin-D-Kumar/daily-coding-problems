82f09349ec3ff3aa62dc98e8a6cb8da03bf7641b
Jatin Kumar <jk8826984588@gmail.com>
2025-08-01 21:18:00 +0530

    Solved problem: Count Common Words With One Occurrence - 1

--- problems/Count_Common_Words_With_One_Occurrence_2025-08-01.js
+++ problems/Count_Common_Words_With_One_Occurrence_2025-08-01.js
@@ -6,4 +6,9 @@
  *
  * Example 1:
  *
- * Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"
+ * Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
+ * Output: 2
+ * Explanation:
+ * - "leetcode" appears exactly once in each of the two arrays. We count this string.
+ * - "amazing" appears exactly once in each of the two arrays. We count this string.
+ * - "is" appears in each of the two arrays, but 
