42b6df718ff11c2b706bf8a6f3c1123bb821bc8f
Jatin Kumar <jk8826984588@gmail.com>
2025-08-01 21:17:43 +0530

    Solved problem: Count Common Words With One Occurrence - 0

---
+++ problems/Count_Common_Words_With_One_Occurrence_2025-08-01.js
@@ -0,0 +1,9 @@
+
+/**
+ * Given two string arrays words1 and words2, return the number of strings that appear exactly once in each of the two arrays.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"
