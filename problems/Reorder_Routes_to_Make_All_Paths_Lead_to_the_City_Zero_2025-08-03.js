385e8b77eb9d6b37b044e5844f21edbd7612bcb2
Jatin Kumar <jk8826984588@gmail.com>
2025-08-03 22:25:36 +0530

    Solved problem: Reorder Routes to Make All Paths Lead to the City Zero - 1

--- problems/Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero_2025-08-03.js
+++ problems/Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero_2025-08-03.js
@@ -3,4 +3,7 @@ function minReorder(n, connections) {
     // Create an adjacency list to represent the graph
     const graph = new Map();
     
+    // Populate the graph with the connections
+    for (const [a, b] of connections) {
+        if (!graph.has(a)) graph.set(a, []);
     
