710ad9508f6ab31851603a9f8b65aaaa11de61ec
Jatin Kumar <jk8826984588@gmail.com>
2025-08-05 22:33:02 +0530

    Solved problem: Find the Integer Added to Array II - 6

--- problems/Find_the_Integer_Added_to_Array_II_2025-08-05.js
+++ problems/Find_the_Integer_Added_to_Array_II_2025-08-05.js
@@ -16,4 +16,5 @@ function findAddedInteger(arr, addedInt) {
 // Example usage:
 const originalArray = [1, 2, 3, 2, 1];
 const integerAdded = 4;
-const result = findAddedInteger
+const result = findAddedInteger(originalArray, integerAdded);
+console.log(result); // Output: 4
