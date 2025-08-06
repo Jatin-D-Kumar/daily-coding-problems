5ca2e98626ac6993cc34510e1e03ca66d881a24a
Jatin Kumar <jk8826984588@gmail.com>
2025-08-06 22:00:08 +0530

    Solved problem: Word Subsets - 1

--- problems/Word_Subsets_2025-08-06.js
+++ problems/Word_Subsets_2025-08-06.js
@@ -3,4 +3,7 @@ function wordSubsets(A, B) {
     // Create a frequency count for all characters needed from B
     const freqB = Array(26).fill(0);
     
-    for (co
+    for (const word of B) {
+        const count = Array(26).fill(0);
+        for (const char of word) {
+            count[char.charCodeAt(0) - 'a'.charCodeAt(0
