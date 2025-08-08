4c5a1e50f5ce980f3a42ee1f02ace2f85a830dbe
Jatin Kumar <jk8826984588@gmail.com>
2025-08-08 20:20:22 +0530

    Solved problem: Select Data - 8

--- problems/Select_Data_2025-08-08.js
+++ problems/Select_Data_2025-08-08.js
@@ -3,4 +3,6 @@ function selectData(data, criteria) {
     return data.filter(item => 
         Object.keys(criteria).every(key => 
             item[key] === criteria[key]
-     
+        )
+    );
+}
