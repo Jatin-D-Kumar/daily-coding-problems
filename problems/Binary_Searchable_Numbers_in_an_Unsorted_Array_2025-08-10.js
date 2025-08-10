a1027c9a279b0ae7c6481eb80faeb3c28818bae6
Jatin Kumar <jk8826984588@gmail.com>
2025-08-10 21:51:10 +0530

    Solved problem: Binary Searchable Numbers in an Unsorted Array - 1

--- problems/Binary_Searchable_Numbers_in_an_Unsorted_Array_2025-08-10.js
+++ problems/Binary_Searchable_Numbers_in_an_Unsorted_Array_2025-08-10.js
@@ -1,4 +1,8 @@
 
 function findBinarySearchableNumbers(arr) {
     const n = arr.length;
-    const leftMax = new Array
+    const leftMax = new Array(n).fill(-Infinity);
+    const rightMin = new Array(n).fill(Infinity);
+    const result = [];
+
+    /
