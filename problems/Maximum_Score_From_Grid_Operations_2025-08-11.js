f52bef342f85f914a49857b204951849098a1c17
Jatin Kumar <jk8826984588@gmail.com>
2025-08-11 22:23:50 +0530

    Solved problem: Maximum Score From Grid Operations - 1

--- problems/Maximum_Score_From_Grid_Operations_2025-08-11.js
+++ problems/Maximum_Score_From_Grid_Operations_2025-08-11.js
@@ -1,4 +1,7 @@
 
 function maxScore(grid) {
     let m = grid.length;       // number of rows
-    let n = gri
+    let n = grid[0].length;    // number of columns
+    let score = 0;
+
+    // This approach assumes we ca
