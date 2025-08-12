0dcb42bd181c684fd4f574fd39ac6ed07f3d541a
Jatin Kumar <jk8826984588@gmail.com>
2025-08-12 22:30:35 +0530

    Solved problem: Additive Number - 1

--- problems/Additive_Number_2025-08-12.js
+++ problems/Additive_Number_2025-08-12.js
@@ -5,3 +5,8 @@ function isAdditiveNumber(num) {
     // Helper function to check if the sequence is valid
     function isValidSequence(start, first, second) {
         let sum = first + second;
+        let sumStr = sum.toString();
+        let nextStart = start + sumStr.length;
+
+        // If the next part of the string doesn't match the sum, return false
+        if (nextStart > n || num.substring
