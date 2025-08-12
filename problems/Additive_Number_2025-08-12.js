232f585b747fa91cda78bff19cd3c2e2395c2419
Jatin Kumar <jk8826984588@gmail.com>
2025-08-12 22:30:48 +0530

    Solved problem: Additive Number - 2

--- problems/Additive_Number_2025-08-12.js
+++ problems/Additive_Number_2025-08-12.js
@@ -9,4 +9,11 @@ function isAdditiveNumber(num) {
         let nextStart = start + sumStr.length;
 
         // If the next part of the string doesn't match the sum, return false
-        if (nextStart > n || num.substring
+        if (nextStart > n || num.substring(start, nextStart) !== sumStr) {
+            return false;
+        }
+
+        // If we reach the end of the string, we have a valid sequence
+        if (nextStart === n) {
+            return true;
+        
