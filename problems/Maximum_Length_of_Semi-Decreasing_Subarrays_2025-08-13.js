0b7a4dbc717cb1518525313b6c14d96b53243130
Jatin Kumar <jk8826984588@gmail.com>
2025-08-13 21:06:45 +0530

    Solved problem: Maximum Length of Semi-Decreasing Subarrays - 1

--- problems/Maximum_Length_of_Semi-Decreasing_Subarrays_2025-08-13.js
+++ problems/Maximum_Length_of_Semi-Decreasing_Subarrays_2025-08-13.js
@@ -5,4 +5,8 @@ function maxLengthOfSemiDecreasingSubarrays(arr) {
     let maxLength = 1; // At least one element can form a semi-decreasing subarray
     let currentLength = 1;
 
-    // Iterate th
+    // Iterate through the array starting from the second element
+    for (let i = 1; i < arr.length; i++) {
+        // Check if the current element is less than or equal to the previous element
+        if (arr[i] <= arr[i - 1]) {
+   
