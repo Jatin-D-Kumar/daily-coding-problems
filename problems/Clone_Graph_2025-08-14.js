7b27ace6c9cf463da30f2fd8a8500c8393f4ed11
Jatin Kumar <jk8826984588@gmail.com>
2025-08-14 21:23:46 +0530

    Solved problem: Clone Graph - 4

--- problems/Clone_Graph_2025-08-14.js
+++ problems/Clone_Graph_2025-08-14.js
@@ -37,4 +37,14 @@ const node2 = new Node(2);
 const node3 = new Node(3);
 const node4 = new Node(4);
 
-node1.neighbors.pus
+node1.neighbors.push(node2);
+node1.neighbors.push(node4);
+node2.neighbors.push(node1);
+node2.neighbors.push(node3);
+node3.neighbors.push(node2);
+node3.neighbors.push(node4);
+node4.neighbors.push(node1);
+node4.neighbors.push(node3);
+
+const clonedGraph = cloneGraph(node1);
+console.log(clonedGraph);
