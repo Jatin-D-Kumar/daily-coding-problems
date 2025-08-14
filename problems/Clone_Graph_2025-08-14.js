e9fbe956e182467851f268a03bda397476bb2426
Jatin Kumar <jk8826984588@gmail.com>
2025-08-14 21:23:15 +0530

    Solved problem: Clone Graph - 1

--- problems/Clone_Graph_2025-08-14.js
+++ problems/Clone_Graph_2025-08-14.js
@@ -9,4 +9,12 @@ class Node {
 function cloneGraph(node) {
     if (!node) return null; // If the input node is null, return null
 
-    const visited = new Map(); // Map to keep track of the vis
+    const visited = new Map(); // Map to keep track of the visited nodes
+
+    function dfs(currentNode) {
+        if (visited.has(currentNode)) {
+            return visited.get(currentNode); // Return the cloned node if it’s already visited
+        }
+
+        // Create a new node for the current node
+        const cloneNode = new Node(currentN
