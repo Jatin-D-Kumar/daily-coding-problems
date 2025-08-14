43b026328cc255eeda1733ac55e5fa3c7f87559e
Jatin Kumar <jk8826984588@gmail.com>
2025-08-14 21:18:30 +0530

    Solved problem: Maximum Building Height - 1

--- problems/Maximum_Building_Height_2025-08-14.js
+++ problems/Maximum_Building_Height_2025-08-14.js
@@ -5,4 +5,9 @@ function maxBuilding(n, restrictions) {
     restrictions.push([n, 0]);
     
     // Sort restrictions by position
-   
+    restrictions.sort((a, b) => a[0] - b[0]);
+
+    let m = restrictions.length;
+    // Propagate height constraints from left to right
+    for (let i = 1; i < m; i++) {
+        con
