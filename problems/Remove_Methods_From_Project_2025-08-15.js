c5faa594ebc2a86470edfdb477c36f9298f7926b
Jatin Kumar <jk8826984588@gmail.com>
2025-08-15 21:28:07 +0530

    Solved problem: Remove Methods From Project - 1

--- problems/Remove_Methods_From_Project_2025-08-15.js
+++ problems/Remove_Methods_From_Project_2025-08-15.js
@@ -5,4 +5,13 @@ const project = {
         console.log('Method 1');
     },
     method2: function() {
-        console.log('Method 2')
+        console.log('Method 2');
+    },
+    method3: function() {
+        console.log('Method 3');
+    }
+};
+
+// Function to remove specified methods from the object
+function removeMethods(obj, methodsToRemove) {
+ 
