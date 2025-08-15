161ad299c37360360fda2fbeede348f12d64da00
Jatin Kumar <jk8826984588@gmail.com>
2025-08-15 21:27:50 +0530

    Solved problem: Remove Methods From Project - 0

---
+++ problems/Remove_Methods_From_Project_2025-08-15.js
@@ -0,0 +1,8 @@
+
+// Example object with multiple methods
+const project = {
+    method1: function() {
+        console.log('Method 1');
+    },
+    method2: function() {
+        console.log('Method 2')
