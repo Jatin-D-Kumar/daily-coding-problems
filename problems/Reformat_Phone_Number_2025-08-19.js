4160eeb681696b34a1f6d12f3709deaee8eff5f3
Jatin Kumar <jk8826984588@gmail.com>
2025-08-19 22:30:09 +0530

    Solved problem: Reformat Phone Number - 7

--- problems/Reformat_Phone_Number_2025-08-19.js
+++ problems/Reformat_Phone_Number_2025-08-19.js
@@ -33,4 +33,6 @@ function reformatPhoneNumber(phone) {
 
 // Example usage:
 console.log(reformatPhoneNumber("1-23-45 6")); // Output: "123-456"
-console.log(reformatPhoneNumber("123 4-567")); // Outp
+console.log(reformatPhoneNumber("123 4-567")); // Output: "123-45-67"
+console.log(reformatPhoneNumber("123 4-56"));  // Output: "123-456"
+console.log(reformatPhoneNumber("123"));        // Output: "123"
