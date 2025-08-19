e38d83ce0cda3db83ccbdf0cfa8f89ceb640522f
Jatin Kumar <jk8826984588@gmail.com>
2025-08-19 22:28:47 +0530

    Solved problem: Reformat Phone Number - 1

--- problems/Reformat_Phone_Number_2025-08-19.js
+++ problems/Reformat_Phone_Number_2025-08-19.js
@@ -3,4 +3,9 @@ function reformatPhoneNumber(phone) {
     // Remove all non-digit characters
     const digits = phone.replace(/\D/g, '');
     let result = '';
-    l
+    let i = 0;
+
+    while (i < digits.length) {
+        const remainingDigits = digits.length - i;
+
+        // If there are 4 digits left, we need to spli
