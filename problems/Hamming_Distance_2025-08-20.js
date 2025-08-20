eeda2d42b6fd416ed4d880db9e7430b5ec6c09c3
Jatin Kumar <jk8826984588@gmail.com>
2025-08-20 22:29:57 +0530

    Solved problem: Hamming Distance - 2

--- problems/Hamming_Distance_2025-08-20.js
+++ problems/Hamming_Distance_2025-08-20.js
@@ -2,4 +2,10 @@
 function hammingDistance(str1, str2) {
     // Check if the strings are of the same length
     if (str1.length !== str2.length) {
-        throw new Error("Strings must be of the same
+        throw new Error("Strings must be of the same length");
+    }
+
+    let distance = 0;
+
+    // Compare each character in the strings
+    f
