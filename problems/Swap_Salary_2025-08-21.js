dd45f2538942cdacadbb67726792dc19454517f1
Jatin Kumar <jk8826984588@gmail.com>
2025-08-21 21:09:59 +0530

    Solved problem: Swap Salary - 2

--- problems/Swap_Salary_2025-08-21.js
+++ problems/Swap_Salary_2025-08-21.js
@@ -4,4 +4,7 @@ function swapSalary(employees, nameA, nameB) {
     const indexA = employees.findIndex(employee => employee.name === nameA);
     const indexB = employees.findIndex(employee => employee.name === nameB);
     
-    // Check if both emplo
+    // Check if both employees exist
+    if (indexA === -1 || indexB === -1) {
+        console.log("One or both employees not found.");
+        return employees; // re
