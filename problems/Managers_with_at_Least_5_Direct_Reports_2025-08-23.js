362d18d3ab3eca0bf4b71b7cc96bc9abc38a7048
Jatin Kumar <jk8826984588@gmail.com>
2025-08-23 21:58:37 +0530

    Solved problem: Managers with at Least 5 Direct Reports - 0

---
+++ problems/Managers_with_at_Least_5_Direct_Reports_2025-08-23.js
@@ -0,0 +1,6 @@
+
+const employees = [
+    { id: 1, name: 'Alice', managerId: null }, // CEO
+    { id: 2, name: 'Bob', managerId: 1 },
+    { id: 3, name: 'Carol', managerId: 1 },
+    { id: 4, name: 'D
