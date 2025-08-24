976d61ed31a1e6741315c2e1282511aeec4358d3
Jatin Kumar <jk8826984588@gmail.com>
2025-08-24 23:00:33 +0530

    Solved problem: Find Loyal Customers - 6

--- problems/Find_Loyal_Customers_2025-08-24.js
+++ problems/Find_Loyal_Customers_2025-08-24.js
@@ -5,4 +5,5 @@ const purchases = [
     { customerId: 'A123', date: '2023-01-10' },
     { customerId: 'B456', date: '2023-01-02' },
     { customerId: 'B456', date: '2023-01-15' },
-    { customerId: 'C789', date: '2023-01-01' 
+    { customerId: 'C789', date: '2023-01-01' },
+    { customerId: 'C789', date: '2023-01-02' },
