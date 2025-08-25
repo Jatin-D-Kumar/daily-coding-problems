e5564cc94c5c3ed18ed2ad7f99bfb3a36bc70dc2
Jatin Kumar <jk8826984588@gmail.com>
2025-08-25 22:45:08 +0530

    Solved problem: A Number After a Double Reversal - 1

--- problems/A_Number_After_a_Double_Reversal_2025-08-25.js
+++ problems/A_Number_After_a_Double_Reversal_2025-08-25.js
@@ -3,4 +3,8 @@ function doubleReversal(n) {
     // Convert the number to string to manipulate it
     const strNum = n.toString();
 
-    // Reverse 
+    // Reverse the string
+    const reversedOnce = strNum.split('').reverse().join('');
+
+    // Reverse it again
+    const reversedTwice = reversed
