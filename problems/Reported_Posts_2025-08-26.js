3bae7ef78e0c59ac60d6143c9c31c3a538e4787a
Jatin Kumar <jk8826984588@gmail.com>
2025-08-26 21:43:12 +0530

    Solved problem: Reported Posts - 1

--- problems/Reported_Posts_2025-08-26.js
+++ problems/Reported_Posts_2025-08-26.js
@@ -3,4 +3,14 @@ class Post {
     constructor(id, content) {
         this.id = id;
         this.content = content;
-        this.reported = fals
+        this.reported = false;
+    }
+}
+
+class ReportedPostsManager {
+    constructor() {
+        this.posts = [];
+    }
+
+    addPost(content) {
+        con
