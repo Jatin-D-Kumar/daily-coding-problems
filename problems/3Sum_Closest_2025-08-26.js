985c2dd0d20f14fd4721b6d3c5fc0b7d7a0eb553
Jatin Kumar <jk8826984588@gmail.com>
2025-08-26 21:48:38 +0530

    Solved problem: 3Sum Closest - 1

--- problems/3Sum_Closest_2025-08-26.js
+++ problems/3Sum_Closest_2025-08-26.js
@@ -4,4 +4,9 @@ function threeSumClosest(nums, target) {
     nums.sort((a, b) => a - b);
     let closestSum = Infinity;
 
-    for (let i = 0; i < num
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const currentSum = nums[i] 
