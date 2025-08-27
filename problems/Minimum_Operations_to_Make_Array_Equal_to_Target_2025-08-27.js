d94fc8ca9a2a952191913ce39d3e51f901a34789
Jatin Kumar <jk8826984588@gmail.com>
2025-08-27 21:58:25 +0530

    Solved problem: Minimum Operations to Make Array Equal to Target - 4

--- problems/Minimum_Operations_to_Make_Array_Equal_to_Target_2025-08-27.js
+++ problems/Minimum_Operations_to_Make_Array_Equal_to_Target_2025-08-27.js
@@ -3,4 +3,10 @@ function minOperations(arr, target) {
     let operations = 0;
 
     for (let i = 0; i < arr.length; i++) {
-        operations += Math.abs(arr[i] - targe
+        operations += Math.abs(arr[i] - target);
+    }
+
+    return operations;
+}
+
+/
