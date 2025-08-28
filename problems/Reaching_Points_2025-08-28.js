ee8092b7492d6057ce24f5406d73883a8cb28158
Jatin Kumar <jk8826984588@gmail.com>
2025-08-28 21:15:30 +0530

    Solved problem: Reaching Points - 1

--- problems/Reaching_Points_2025-08-28.js
+++ problems/Reaching_Points_2025-08-28.js
@@ -1,4 +1,8 @@
 
 function canReach(sx, sy, tx, ty) {
     while (tx >= sx && ty >= sy) {
-        if (tx === sx && ty === s
+        if (tx === sx && ty === sy) {
+            return true;
+        }
+        // If ty > tx, we check if we can reduce ty
+        if (t
