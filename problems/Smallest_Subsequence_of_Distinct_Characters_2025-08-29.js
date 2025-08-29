810ae8f3cd41aaca4802f3d68131b245cba992db
Jatin Kumar <jk8826984588@gmail.com>
2025-08-29 22:12:42 +0530

    Solved problem: Smallest Subsequence of Distinct Characters - 1

--- problems/Smallest_Subsequence_of_Distinct_Characters_2025-08-29.js
+++ problems/Smallest_Subsequence_of_Distinct_Characters_2025-08-29.js
@@ -4,4 +4,12 @@ function smallestSubsequence(s) {
     const seen = {};
     const result = [];
 
-    // Record the last occurrence of each character in t
+    // Record the last occurrence of each character in the string
+    for (let i = 0; i < s.length; i++) {
+        lastOccurrence[s[i]] = i;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+
+      
