450a0c2066aa74a7ebc48b31873fb001aee992c9
Jatin Kumar <jk8826984588@gmail.com>
2025-08-30 21:10:22 +0530

    Solved problem: Count Beautiful Numbers - 1

--- problems/Count_Beautiful_Numbers_2025-08-30.js
+++ problems/Count_Beautiful_Numbers_2025-08-30.js
@@ -1,3 +1,6 @@
 
 function countBeautifulNumbers(n, k) {
-    // Initialize a counter for b
+    // Initialize a counter for beautiful numbers
+    let count = 0;
+
+    // Iterate through the range fro
