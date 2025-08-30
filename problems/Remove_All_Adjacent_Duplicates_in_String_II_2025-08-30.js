75b52418ee5781f017d98a4f09b796ae9cc4bb9f
Jatin Kumar <jk8826984588@gmail.com>
2025-08-30 21:05:08 +0530

    Solved problem: Remove All Adjacent Duplicates in String II - 2

--- problems/Remove_All_Adjacent_Duplicates_in_String_II_2025-08-30.js
+++ problems/Remove_All_Adjacent_Duplicates_in_String_II_2025-08-30.js
@@ -6,4 +6,10 @@ function removeDuplicates(s, k) {
         // Check if the stack is not empty and the last element is the same as the current character
         if (stack.length > 0 && stack[stack.length - 1][0] === char) {
             // Increment the count of the last character in the stack
-            stack[stac
+            stack[stack.length - 1][1]++;
+        } else {
+            // Push the new character onto the stack with a count of 1
+            stack.push([char, 1]);
+        }
+
+        // If the co
