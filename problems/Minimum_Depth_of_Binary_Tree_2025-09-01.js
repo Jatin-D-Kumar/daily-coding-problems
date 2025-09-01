20526f769e7ac4a98a68be7c17679fca97e10a74
Jatin Kumar <jk8826984588@gmail.com>
2025-09-01 21:31:56 +0530

    Solved problem: Minimum Depth of Binary Tree - 1

--- problems/Minimum_Depth_of_Binary_Tree_2025-09-01.js
+++ problems/Minimum_Depth_of_Binary_Tree_2025-09-01.js
@@ -3,4 +3,13 @@
 class TreeNode {
     constructor(val = 0, left = null, right = null) {
         this.val = val;
-        this
+        this.left = left;
+        this.right = right;
+    }
+}
+
+function minDepth(root) {
+    // Edge case: if the tree is empty
+    if (!root) return 0;
+
+    
