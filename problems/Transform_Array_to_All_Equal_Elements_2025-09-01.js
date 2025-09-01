c5a5319f2bb0de8488253a6e02c714eac746132a
Jatin Kumar <jk8826984588@gmail.com>
2025-09-01 21:27:55 +0530

    Solved problem: Transform Array to All Equal Elements - 1

--- problems/Transform_Array_to_All_Equal_Elements_2025-09-01.js
+++ problems/Transform_Array_to_All_Equal_Elements_2025-09-01.js
@@ -4,4 +4,8 @@ function canTransformToEqual(arr) {
 
     const n = arr.length;
     const sum = arr.reduce((a, b) => a + b, 0);
-    const targetValue = sum / 
+    const targetValue = sum / n;
+
+    // To transform the array to all elements equal to targetValue,
+    // the targetValue should be a whole number.
+    // If it's not, it's not possible to make all elements eq
