7f26e23f73306ff19ac2d1295a878f61d210c43e
Jatin Kumar <jk8826984588@gmail.com>
2025-09-01 21:28:24 +0530

    Solved problem: Transform Array to All Equal Elements - 2

--- problems/Transform_Array_to_All_Equal_Elements_2025-09-01.js
+++ problems/Transform_Array_to_All_Equal_Elements_2025-09-01.js
@@ -8,4 +8,10 @@ function canTransformToEqual(arr) {
 
     // To transform the array to all elements equal to targetValue,
     // the targetValue should be a whole number.
-    // If it's not, it's not possible to make all elements eq
+    // If it's not, it's not possible to make all elements equal.
+    if (targetValue % 1 !== 0) return false;
+
+    // Check if we can transform each element to targetValue
+    for (let num of arr) {
+        if (num !== targetValue) {
+        
