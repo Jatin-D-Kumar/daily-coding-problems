914309c2dfc5377d4dbd6d12e92dea16c36131dc
Jatin Kumar <jk8826984588@gmail.com>
2025-09-03 21:20:32 +0530

    Solved problem: Validate Binary Search Tree - 0

---
+++ problems/Validate_Binary_Search_Tree_2025-09-03.js
@@ -0,0 +1,11 @@
+
+class TreeNode {
+    constructor(value) {
+        this.val = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function isValidBST(root) {
+    // Helper function to validate the B
