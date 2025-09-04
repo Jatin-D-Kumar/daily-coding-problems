69b484972c99298a5ff3ae4bb9253a390b0c94bd
Jatin Kumar <jk8826984588@gmail.com>
2025-09-04 21:58:29 +0530

    Solved problem: Cherry Pickup - 1

--- problems/Cherry_Pickup_2025-09-04.js
+++ problems/Cherry_Pickup_2025-09-04.js
@@ -4,4 +4,8 @@ function cherryPickup(grid) {
     const dp = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
 
     for (let i = n - 1; i >= 0; i--) {
-       
+        for (let j = n - 1; j >= 0; j--) {
+            if (grid[i][j] === -1) {
+                dp[i][j] = -1; // Mark as unreachable
+            } else {
+                // Current cell
