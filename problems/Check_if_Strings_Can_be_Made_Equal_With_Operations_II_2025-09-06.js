0b8014e93ebe9dd745cef687bc6d7e4892fd25b1
Jatin Kumar <jk8826984588@gmail.com>
2025-09-06 21:57:15 +0530

    Solved problem: Check if Strings Can be Made Equal With Operations II - 4

--- problems/Check_if_Strings_Can_be_Made_Equal_With_Operations_II_2025-09-06.js
+++ problems/Check_if_Strings_Can_be_Made_Equal_With_Operations_II_2025-09-06.js
@@ -24,4 +24,11 @@ function canBeMadeEqual(str1, str2) {
         }
     }
 
-    //
+    // Sort both strings and compare
+    const sortedStr1 = str1.split('').sort().join('');
+    const sortedStr2 = str2.split('').sort().join('');
+
+    return sortedStr1 === sortedStr2;
+}
+
+//
