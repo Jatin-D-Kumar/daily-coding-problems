c7a98d605e0c3aa9288d9c7569b238ffc60fe6e1
Jatin Kumar <jk8826984588@gmail.com>
2025-09-06 21:56:43 +0530

    Solved problem: Check if Strings Can be Made Equal With Operations II - 1

--- problems/Check_if_Strings_Can_be_Made_Equal_With_Operations_II_2025-09-06.js
+++ problems/Check_if_Strings_Can_be_Made_Equal_With_Operations_II_2025-09-06.js
@@ -3,4 +3,12 @@ function canBeMadeEqual(str1, str2) {
     // Helper function to count character frequencies
     const getCharFrequency = (str) => {
         const freq = {};
-        for (let char of st
+        for (let char of str) {
+            freq[char] = (freq[char] || 0) + 1;
+        }
+        return freq;
+    };
+
+    // Get character frequencies for both strings
+    const freq1 = getCharFrequency(str1);
+ 
