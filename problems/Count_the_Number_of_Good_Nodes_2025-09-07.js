012e7827420240dc8e9a781176dd938aec9889fe
Jatin Kumar <jk8826984588@gmail.com>
2025-09-07 19:56:35 +0530

    Solved problem: Count the Number of Good Nodes - 1

--- problems/Count_the_Number_of_Good_Nodes_2025-09-07.js
+++ problems/Count_the_Number_of_Good_Nodes_2025-09-07.js
@@ -2,4 +2,13 @@
 function TreeNode(val, left, right) {
     this.val = (val === undefined ? 0 : val);
     this.left = (left === undefined ? null : left);
-    this.right = (right === undefined ? null 
+    this.right = (right === undefined ? null : right);
+}
+
+var goodNodes = function(root) {
+    let count = 0;
+
+    const dfs = (node, maxSoFar) => {
+        if (!node) return;
+
+        // If the current node's value is greater 
