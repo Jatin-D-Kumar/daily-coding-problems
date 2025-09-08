dad59ef35e56a5bab703de47d0a3c748afb8ffdd
Jatin Kumar <jk8826984588@gmail.com>
2025-09-08 21:00:30 +0530

    Solved problem: Solve the Equation - 0

---
+++ problems/Solve_the_Equation_2025-09-08.js
@@ -0,0 +1,7 @@
+
+function solveEquation(equation) {
+    // Split the equation into left and right parts
+    const [left, right] = equation.split('=');
+    
+    // Function to parse the expression and return coefficients
+    fu
