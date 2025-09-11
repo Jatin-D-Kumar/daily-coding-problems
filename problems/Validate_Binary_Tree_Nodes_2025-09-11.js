51735a95b7dd70c09f43e3ecc0af2ff3836ebcd9
Jatin Kumar <jk8826984588@gmail.com>
2025-09-11 21:17:24 +0530

    Solved problem: Validate Binary Tree Nodes - 1

--- problems/Validate_Binary_Tree_Nodes_2025-09-11.js
+++ problems/Validate_Binary_Tree_Nodes_2025-09-11.js
@@ -3,4 +3,8 @@ function validateBinaryTreeNodes(n, leftChild, rightChild) {
     // Array to keep track of the number of parents for each node
     const parentCount = new Array(n).fill(0);
 
-    
+    // Loop through all nodes to count parents
+    for (let i = 0; i < n; i++) {
+        // Check left child
+        if (leftChild[i] !== -1) {
+            parentCount[leftChild[i]]++
