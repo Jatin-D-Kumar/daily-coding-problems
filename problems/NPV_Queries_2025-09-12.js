1c001cf455637b83051dd37d6c20d25fd87a0417
Jatin Kumar <jk8826984588@gmail.com>
2025-09-12 21:32:26 +0530

    Solved problem: NPV Queries - 1

--- problems/NPV_Queries_2025-09-12.js
+++ problems/NPV_Queries_2025-09-12.js
@@ -2,4 +2,7 @@
 /**
  * Calculate the Net Present Value (NPV)
  * @param {number[]} cashFlows - Array of cash flows (can be positive or negative)
- * @param {number} dis
+ * @param {number} discountRate - The discount rate (as a decimal, e.g., 10% as 0.1)
+ * @return {number} - The calculated NPV
+ */
+function calculateNPV(cashFlows, discountRa
