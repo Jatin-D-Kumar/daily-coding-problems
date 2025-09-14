58323da7dbdbc364adc9bf26f23aa15b20df014c
Jatin Kumar <jk8826984588@gmail.com>
2025-09-14 17:07:53 +0530

    Solved problem: Number of Subarrays with Bounded Maximum - 0

---
+++ problems/Number_of_Subarrays_with_Bounded_Maximum_2025-09-14.js
@@ -0,0 +1,4 @@
+
+function numSubarrayBoundedMax(nums, left, right) {
+    let count = 0; // This will hold the total count of valid subarrays
+    let totalValidSubarrays = 0; // This will count valid subarrays
