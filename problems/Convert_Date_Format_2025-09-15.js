67098fa8af2b004fb0a5a088eedf322591da6c24
Jatin Kumar <jk8826984588@gmail.com>
2025-09-15 19:23:38 +0530

    Solved problem: Convert Date Format - 2

--- problems/Convert_Date_Format_2025-09-15.js
+++ problems/Convert_Date_Format_2025-09-15.js
@@ -3,4 +3,8 @@ function convertDateFormat(dateString) {
     // Check if the input is a valid date string in "YYYY-MM-DD" format
     const regex = /^\d{4}-\d{2}-\d{2}$/;
     if (!regex.test(dateString)) {
-        throw new Error("Invalid
+        throw new Error("Invalid date format. Please use 'YYYY-MM-DD'.");
+    }
+
+    // Split the date string into an array [YYYY, MM, DD]
+    
