bc4f452752fb1c62ce74f102b1e8693fe678582c
Jatin Kumar <jk8826984588@gmail.com>
2025-09-16 22:07:37 +0530

    Solved problem: NPV Queries - 1

--- problems/NPV_Queries_2025-09-16.js
+++ problems/NPV_Queries_2025-09-16.js
@@ -5,3 +5,15 @@
  * @param {number} discountRate - Discount rate (as a decimal).
  * @returns {number} - The computed NPV.
  */
+function calculateNPV(cashFlows, discountRate) {
+    let npv = 0;
+
+    for (let t = 0; t < cashFlows.length; t++) {
+        npv += cashFlows[t] / Math.pow(1 + discountRate, t);
+    }
+
+    return npv;
+}
+
+/**
+ * Handles multiple NPV 
