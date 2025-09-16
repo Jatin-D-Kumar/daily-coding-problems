58e29e20efd43bfc7bc78190a1787b3a6056a728
Jatin Kumar <jk8826984588@gmail.com>
2025-09-16 22:07:26 +0530

    Solved problem: NPV Queries - 0

---
+++ problems/NPV_Queries_2025-09-16.js
@@ -0,0 +1,7 @@
+
+/**
+ * Function to calculate NPV given cash flows and discount rate.
+ * @param {number[]} cashFlows - Array of cash flows.
+ * @param {number} discountRate - Discount rate (as a decimal).
+ * @returns {number} - The computed NPV.
+ */
