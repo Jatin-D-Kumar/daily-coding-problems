8a6422e752ba415f3417d8f15f0fa86cca71992b
Jatin Kumar <jk8826984588@gmail.com>
2025-09-18 21:50:13 +0530

    Solved problem: The Latest Time to Catch a Bus - 0

---
+++ problems/The_Latest_Time_to_Catch_a_Bus_2025-09-18.js
@@ -0,0 +1,7 @@
+
+/**
+ * The Latest Time to Catch a Bus
+ * @param {number[]} bus        - sorted ascending bus arrival times
+ * @param {number[]} passenger - sorted ascending passenger arrival times
+ * @param {number} capacity     - maximum passengers per bus
+ * @re
