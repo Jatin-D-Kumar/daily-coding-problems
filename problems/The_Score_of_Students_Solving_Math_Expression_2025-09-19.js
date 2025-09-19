46e8c2a085d76f5cef28fd314bf3020708a384a5
Jatin Kumar <jk8826984588@gmail.com>
2025-09-19 21:45:45 +0530

    Solved problem: The Score of Students Solving Math Expression - 1

--- problems/The_Score_of_Students_Solving_Math_Expression_2025-09-19.js
+++ problems/The_Score_of_Students_Solving_Math_Expression_2025-09-19.js
@@ -2,3 +2,9 @@
 function scoreStudents(equation, studentAnswers) {
     // Evaluate the correct answer by evaluating the equation string
     const correctAnswer = eval(equation);
+    
+    // To keep track of scores
+    const scores = [];
+
+    studentAnswers.forEach(answer => {
+        if (typeof answer === 'number' && answer === correctAnsw
