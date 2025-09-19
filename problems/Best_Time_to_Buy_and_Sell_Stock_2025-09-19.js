8a10908b60bde56e72104dd057c7bff3b2a6eb05
Jatin Kumar <jk8826984588@gmail.com>
2025-09-19 21:50:13 +0530

    Solved problem: Best Time to Buy and Sell Stock - 1

--- problems/Best_Time_to_Buy_and_Sell_Stock_2025-09-19.js
+++ problems/Best_Time_to_Buy_and_Sell_Stock_2025-09-19.js
@@ -1,4 +1,8 @@
 
 function maxProfit(prices) {
     // Initialize the minimum price and maximum profit
-    let minPrice = I
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    // Loop through each price in the prices array
+    for (let i = 0; i
