834181dbbe751ec06679a22e8dd8b2786f701434
Jatin Kumar <jk8826984588@gmail.com>
2025-09-20 21:54:19 +0530

    Solved problem: Maximum Sum of Edge Values in a Graph - 1

--- problems/Maximum_Sum_of_Edge_Values_in_a_Graph_2025-09-20.js
+++ problems/Maximum_Sum_of_Edge_Values_in_a_Graph_2025-09-20.js
@@ -2,4 +2,5 @@
 function maximumSumOfEdgeValues(edges) {
     let maxSum = 0;
 
-    for (let edge of edges
+    for (let edge of edges) {
+        // Assume each edge is an object like {u: vertex1, v: vertex2, weight: edgeWe
