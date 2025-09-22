49bf6177b060e5873b85dd97391995042018a117
Jatin Kumar <jk8826984588@gmail.com>
2025-09-22 20:07:23 +0530

    Solved problem: Design Log Storage System - 1

--- problems/Design_Log_Storage_System_2025-09-22.js
+++ problems/Design_Log_Storage_System_2025-09-22.js
@@ -2,4 +2,13 @@
 class LogEntry {
     constructor(timestamp, message) {
         this.timestamp = timestamp; // in milliseconds (Date.now())
-        
+        this.message = message;
+    }
+}
+
+class LogStorage {
+    constructor() {
+        this.logs = [];
+    }
+
+    // Method to add a log en
