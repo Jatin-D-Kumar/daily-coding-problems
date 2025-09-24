52c452707d84aa8001320147171d9af03b650503
Jatin Kumar <jk8826984588@gmail.com>
2025-09-24 21:37:03 +0530

    Solved problem: Find the Maximum Factor Score of Array - 1

--- problems/Find_the_Maximum_Factor_Score_of_Array_2025-09-24.js
+++ problems/Find_the_Maximum_Factor_Score_of_Array_2025-09-24.js
@@ -3,4 +3,7 @@ function sumOfDivisors(n) {
     let sum = 0;
     
     // Loop through numbers to find divisors
-    for (le
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            sum += i; // i is a divisor
+     
