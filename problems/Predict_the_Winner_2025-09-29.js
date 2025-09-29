c14eb5d974ff996cc2524ec5e36a7e02d6cd27b6
Jatin Kumar <jk8826984588@gmail.com>
2025-09-29 21:43:28 +0530

    Solved problem: Predict the Winner - 1

--- problems/Predict_the_Winner_2025-09-29.js
+++ problems/Predict_the_Winner_2025-09-29.js
@@ -1,4 +1,6 @@
 
 function PredictTheWinner(nums) {
     const n = nums.length;
-    // Create a DP table to store the maximum dif
+    // Create a DP table to store the maximum difference in scores
+    // between the current player and the opponent
+    const dp = Array.from({ length: n }, (
