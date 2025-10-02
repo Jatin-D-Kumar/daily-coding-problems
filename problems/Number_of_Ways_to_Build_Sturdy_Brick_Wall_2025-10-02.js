20ad1d01f70a966660cce200db7b1f59c5066bc5
Jatin Kumar <jk8826984588@gmail.com>
2025-10-02 16:16:14 +0530

    Solved problem: Number of Ways to Build Sturdy Brick Wall - 1

--- problems/Number_of_Ways_to_Build_Sturdy_Brick_Wall_2025-10-02.js
+++ problems/Number_of_Ways_to_Build_Sturdy_Brick_Wall_2025-10-02.js
@@ -3,4 +3,10 @@ function numWaysToBuildWall(n, h) {
   // Base case: If the width of the wall is 0, there's 1 way to build it (by doing nothing)
   if (n === 0) return 1;
 
-  // Create a dp arr
+  // Create a dp array to store the number of ways to fill a width
+  const dp = new Array(n + 1).fill(0);
+
+  // Base case: There is 1 way to fill a width of 0 (empty wall)
+  dp[0] = 1;
+
+  // Base
