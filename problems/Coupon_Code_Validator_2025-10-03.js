01e94c552ca5b04262880eb00f0c1ab978accb94
Jatin Kumar <jk8826984588@gmail.com>
2025-10-03 23:23:55 +0530

    Solved problem: Coupon Code Validator - 0

---
+++ problems/Coupon_Code_Validator_2025-10-03.js
@@ -0,0 +1,14 @@
+
+/**
+ * Coupon Code Validator
+ * Author: ChatGPT
+ * Date: 2025-10-03
+ *
+ * Reads coupon codes from stdin, validates each one, and prints 'VALID' or 'INVALID'.
+ */
+
+'use strict';
+
+const fs = require('fs');
+
+// R
