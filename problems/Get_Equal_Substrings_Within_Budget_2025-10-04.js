c256190e120569aa7b341688737fbbc5866e19a3
Jatin Kumar <jk8826984588@gmail.com>
2025-10-04 19:36:29 +0530

    Solved problem: Get Equal Substrings Within Budget - 1

--- problems/Get_Equal_Substrings_Within_Budget_2025-10-04.js
+++ problems/Get_Equal_Substrings_Within_Budget_2025-10-04.js
@@ -8,4 +8,8 @@ const input = fs.readFileSync(0, 'utf8').trim().split(/\s+/);
 
 let k, s;
 
-// input format 
+// input format is assumed to be:
+//   first line : integer k
+//   second line: string s
+// if the first line contains two integers (n k) we ignore n
+if (input.length >= 2 && /^[0-9]+$/.test(input[0]) && !isNaN(parseI
