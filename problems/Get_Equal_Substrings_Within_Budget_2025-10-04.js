65f9debf68de38352c3aaaa0d141e99b775033ee
Jatin Kumar <jk8826984588@gmail.com>
2025-10-04 19:36:15 +0530

    Solved problem: Get Equal Substrings Within Budget - 0

---
+++ problems/Get_Equal_Substrings_Within_Budget_2025-10-04.js
@@ -0,0 +1,11 @@
+
+//  Get Equal Substrings Within Budget
+//  ECMAScript 2021 (Node.js)
+
+'use strict';
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim().split(/\s+/);
+
+let k, s;
+
+// input format 
