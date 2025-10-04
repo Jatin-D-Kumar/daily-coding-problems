8fd97eed366981e15b2a3df6e2180b7263503262
Jatin Kumar <jk8826984588@gmail.com>
2025-10-04 22:45:34 +0530

    Solved problem: Process Tasks Using Servers - 1

--- problems/Process_Tasks_Using_Servers_2025-10-04.js
+++ problems/Process_Tasks_Using_Servers_2025-10-04.js
@@ -16,4 +16,18 @@ class MinHeap {
 
   // Remove and return the smallest value
   pop() {
-   
+    const a = this.data;
+    if (a.length === 0) return undefined;
+    const min = a[0];
+    const last = a.pop();
+    if (a.length > 0) {
+      a[0] = last;
+      this._siftDown(0);
+    }
+    return min;
+  }
+
+  _siftUp(idx) {
+    const a = this.data;
+    while (idx > 0) {
+    
