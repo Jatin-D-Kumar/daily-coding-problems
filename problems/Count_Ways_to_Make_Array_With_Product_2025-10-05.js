712e141871cd2fc6da4e302ff2d3806e261b5acc
Jatin Kumar <jk8826984588@gmail.com>
2025-10-05 20:12:30 +0530

    Solved problem: Count Ways to Make Array With Product - 0

---
+++ problems/Count_Ways_to_Make_Array_With_Product_2025-10-05.js
@@ -0,0 +1,10 @@
+
+/**
+ * Count the number of non‑empty subsets of `nums` whose product equals `target`.
+ *
+ * @param {number[]} nums  - array of positive integers
+ * @param {number} target  - positive integer target product
+ * @returns {number}       - number of subsets with product == target
+ */
+function countWays(nums, target) {
+    // M
