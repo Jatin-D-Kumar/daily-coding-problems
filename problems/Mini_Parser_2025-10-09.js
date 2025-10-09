17413e1dcd9216ecb571bab8d993e31330f1d645
Jatin Kumar <jk8826984588@gmail.com>
2025-10-09 22:14:43 +0530

    Solved problem: Mini Parser - 0

---
+++ problems/Mini_Parser_2025-10-09.js
@@ -0,0 +1,10 @@
+
+/**
+ * Remove all tags from the string and return concatenated text.
+ * @param {string} s
+ * @return {string}
+ */
+function miniParser(s) {
+  /**
+   * Recursively parse the string starting at index `pos`.
+   * @param {number}
