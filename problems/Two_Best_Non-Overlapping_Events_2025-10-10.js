2c0c71e3aa03d36d22eea645e58801fbfc5c4b16
Jatin Kumar <jk8826984588@gmail.com>
2025-10-10 21:03:32 +0530

    Solved problem: Two Best Non-Overlapping Events - 0

---
+++ problems/Two_Best_Non-Overlapping_Events_2025-10-10.js
@@ -0,0 +1,9 @@
+
+/**
+ * Finds the maximum combined value of two non‑overlapping events.
+ *
+ * @param {number[][]} events   Each element: [start, end, value]
+ * @return {number}
+ */
+function maxValue(events) {
+    if (events.leng
