dce36b92c76bd2e0398b6722288a8c37a68e8ae8
Jatin Kumar <jk8826984588@gmail.com>
2025-10-12 18:45:02 +0530

    Solved problem: Maximum Size of a Set After Removals - 1

--- problems/Maximum_Size_of_a_Set_After_Removals_2025-10-12.js
+++ problems/Maximum_Size_of_a_Set_After_Removals_2025-10-12.js
@@ -1,4 +1,7 @@
 
 function maxSetSizeAfterRemovals(nums) {
   // Step 1: Count the frequency of each element in the array
- 
+  const freqMap = new Map();
+  nums.forEach(num => {
+    freqMap.set(num, (freqMap.get(num) || 0) + 1);
+  
