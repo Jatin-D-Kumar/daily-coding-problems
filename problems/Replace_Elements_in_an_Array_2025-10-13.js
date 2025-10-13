04e1b6439d089a2854c3ca9be472dc9bdc457361
Jatin Kumar <jk8826984588@gmail.com>
2025-10-13 21:25:34 +0530

    Solved problem: Replace Elements in an Array - 2

--- problems/Replace_Elements_in_an_Array_2025-10-13.js
+++ problems/Replace_Elements_in_an_Array_2025-10-13.js
@@ -3,3 +3,5 @@ function replaceElements(arr) {
     let maxRight = -1; // Initialize the maxRight variable to -1 (for the last element)
     
     // Iterate through the array in reverse order
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let current = arr[i]; // Store t
