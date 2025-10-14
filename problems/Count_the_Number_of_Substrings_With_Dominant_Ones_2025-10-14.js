1acbb89919cbe8865d1d5fb0a44bce2f5223a75b
Jatin Kumar <jk8826984588@gmail.com>
2025-10-14 22:46:41 +0530

    Solved problem: Count the Number of Substrings With Dominant Ones - 1

--- problems/Count_the_Number_of_Substrings_With_Dominant_Ones_2025-10-14.js
+++ problems/Count_the_Number_of_Substrings_With_Dominant_Ones_2025-10-14.js
@@ -3,4 +3,7 @@ function countDominantSubstrings(s) {
     let balance = 0;
     let count = 0;
     let balanceMap = new Map();
-    balanceM
+    balanceMap.set(0, 1); // We start with a balance of 0 at the "before start" position.
+
+    for (let i = 0; i < s.length; i++) {
+   
