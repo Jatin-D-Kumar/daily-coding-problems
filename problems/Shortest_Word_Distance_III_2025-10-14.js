cdc2241f83c20584826126fb1bd8b95c2671e592
Jatin Kumar <jk8826984588@gmail.com>
2025-10-14 22:42:27 +0530

    Solved problem: Shortest Word Distance III - 1

--- problems/Shortest_Word_Distance_III_2025-10-14.js
+++ problems/Shortest_Word_Distance_III_2025-10-14.js
@@ -1,4 +1,8 @@
 
 var shortestDistance = function(wordsDict, word1, word2) {
     let index1 = -1;
-    let index2 
+    let index2 = -1;
+    let minDistance = Infinity;
+    
+    for (let i = 0; i < wordsDict.length; i++) {
+    
