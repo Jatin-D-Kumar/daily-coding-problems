a887d7dabd8f09c3e033d549e372160993726db6
Jatin Kumar <jk8826984588@gmail.com>
2025-10-16 21:55:00 +0530

    Solved problem: Determine the Winner of a Bowling Game - 1

--- problems/Determine_the_Winner_of_a_Bowling_Game_2025-10-16.js
+++ problems/Determine_the_Winner_of_a_Bowling_Game_2025-10-16.js
@@ -4,4 +4,6 @@ function calculateBowlingScore(frames) {
     let frameIndex = 0;
 
     for (let frame = 0; frame < 10; frame++) {
-     
+        if (frames[frameIndex] === 10) { // Strike
+            totalScore += 10 + frames[frameIndex + 1] + frames[frameIndex + 2];
+            frame
