03509a5315e535f40eb3e589868a2faf293085b5
Jatin Kumar <jk8826984588@gmail.com>
2025-10-26 16:52:11 +0530

    Solved problem: Partition Array for Maximum XOR and AND - 1

--- problems/Partition_Array_for_Maximum_XOR_and_AND_2025-10-26.js
+++ problems/Partition_Array_for_Maximum_XOR_and_AND_2025-10-26.js
@@ -2,4 +2,9 @@
 /*  Partition Array for Maximum XOR and AND
     ----------------------------------------------------
     Reads:  n  followed by n integers
-    Prints: maximal va
+    Prints: maximal value of prefix XOR XOR suffix AND ----------------------------------------------------
+*/
+
+'use strict';
+const fs = require('fs');
+const data = fs.readFileSync(0, 
