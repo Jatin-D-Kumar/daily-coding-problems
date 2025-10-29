07ee40b60cff12c54946d724b715e7e14e01107a
Jatin Kumar <jk8826984588@gmail.com>
2025-10-29 21:11:15 +0530

    Solved problem: Minimum Difficulty of a Job Schedule - 1

--- problems/Minimum_Difficulty_of_a_Job_Schedule_2025-10-29.js
+++ problems/Minimum_Difficulty_of_a_Job_Schedule_2025-10-29.js
@@ -10,3 +10,13 @@ function mnd(jobDifficulty, d) {
     if (n < d) return -1;             // impossible
 
     const INF = Number.POSITIVE_INFINITY;
+
+    // dpPrev[i] – optimal cost for first i jobs in day-1 days
+    let dpPrev = new Array(n + 1).fill(INF);
+    dpPrev[0] = 0;                    // base case
+
+    // iterate over days
+    for (let day = 1; day <= d; day++) {
+        const dpCurr = new Array(n + 1).fill(INF);
+
+        
