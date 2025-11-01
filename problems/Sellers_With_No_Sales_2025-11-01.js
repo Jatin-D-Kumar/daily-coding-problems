0b88dff9faba5b8d349cb776a2ce579e8f9167ce
Jatin Kumar <jk8826984588@gmail.com>
2025-11-01 11:35:27 +0530

    Solved problem: Sellers With No Sales - 1

--- problems/Sellers_With_No_Sales_2025-11-01.js
+++ problems/Sellers_With_No_Sales_2025-11-01.js
@@ -1,4 +1,7 @@
 
 function sellersWithNoSales(sellers, sales) {
     // Create a set of sellers who made a sale
-    let sellersWith
+    let sellersWithSales = new Set();
+
+    // Loop through the sales and add the seller's name to the set
+    sales.forEach(sale => 
