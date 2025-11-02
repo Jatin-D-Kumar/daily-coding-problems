1e6c2bfc72ddc9791434a125b93a4dfd0a19712c
Jatin Kumar <jk8826984588@gmail.com>
2025-11-02 11:53:28 +0530

    Solved problem: Find the Maximum Factor Score of Array - 1

--- problems/Find_the_Maximum_Factor_Score_of_Array_2025-11-02.js
+++ problems/Find_the_Maximum_Factor_Score_of_Array_2025-11-02.js
@@ -8,4 +8,10 @@ const fs = require('fs');
 const input = fs.readFileSync(0, 'utf8').trim().split(/\s+/).map(Number);
 let idx = 0;
 
-// 
+// ----- read input ---------------------------------------------------------
+const T = input[idx++] || 0;          // number of test cases
+const tests = [];
+let globalMaxVal = 0;
+
+for (let t = 0; t < T; ++t) {
+    con
