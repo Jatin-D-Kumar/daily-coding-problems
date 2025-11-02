c8d4b33ffb800c0e3e7cad7f8c1186c1a7a215c5
Jatin Kumar <jk8826984588@gmail.com>
2025-11-02 11:53:17 +0530

    Solved problem: Find the Maximum Factor Score of Array - 0

---
+++ problems/Find_the_Maximum_Factor_Score_of_Array_2025-11-02.js
@@ -0,0 +1,11 @@
+
+/*  Find the Maximum Factor Score of Array
+ *  ECMAScript 2021 (Node.js v12+)
+ */
+
+'use strict';
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim().split(/\s+/).map(Number);
+let idx = 0;
+
+// 
